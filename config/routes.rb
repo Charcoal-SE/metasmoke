7de9b59117034d744a896678538109e8b842ad9d
Undo1 <parker@erwaysoftware.com>
2015-08-20 12:10:25 -0400

    add root route

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
 
   get "dashboard", to: "dashboard#index"
 
+  root to: "dashboard#index"
+
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
