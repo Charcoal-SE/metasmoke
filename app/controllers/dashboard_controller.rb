217022f763fafa594267622048edd386d93a61a6
Undo1 <parker@erwaysoftware.com>
2015-09-01 18:13:37 -0600

    faster

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,7 +2,7 @@ class DashboardController < ApplicationController
 #  before_filter :authenticate_user!
 
   def index
-    @reasons = Reason.all
+    @reasons = Reason.all.joins(:posts).group("reasons.id").select("reasons.*, count('posts.*') as post_count").to_a
     @posts = Post.all
   end
 end
