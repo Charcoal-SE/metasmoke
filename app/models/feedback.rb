4456a435275196fc72dead8270e9f6698e0a5314
Undo1 <parker@erwaysoftware.com>
2015-09-03 16:56:57 -0600

    show feedback

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -1,3 +1,10 @@
 class Feedback < ActiveRecord::Base
   belongs_to :post
+
+  def is_positive?
+    self.feedback_type.include? "tp"
+  end
+  def is_negative?
+    self.feedback_type.include? "fp"
+  end
 end
