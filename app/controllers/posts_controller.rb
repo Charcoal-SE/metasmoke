033ed3abbf2a40ca52e9522cccd13c24e7522efc
Undo1 <parker@erwaysoftware.com>
2015-09-06 20:41:37 -0600

    show site logos

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,6 +6,7 @@ class PostsController < ApplicationController
   # GET /posts.json
   def index
     @posts = Post.all.includes(:reasons).includes(:feedbacks).paginate(:page => params[:page], :per_page => 50).order('created_at DESC')
+    @sites = Site.where(:id => @posts.map(&:site_id))
   end
 
   # POST /posts
