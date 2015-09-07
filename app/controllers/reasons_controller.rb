62412123b8507273ac70ea9a79eaea34764d7cd0
Undo1 <parker@erwaysoftware.com>
2015-09-07 08:39:59 -0600

    I'm an idiot

--- app/controllers/reasons_controller.rb
+++ app/controllers/reasons_controller.rb
@@ -2,5 +2,6 @@ class ReasonsController < ApplicationController
   def show
     @reason = Reason.find(params[:id])
     @posts = @reason.posts.includes(:reasons).includes(:feedbacks).paginate(:page => params[:page], :per_page => 10).order('created_at DESC')
+    @sites = Site.where(:id => @posts.map(&:site_id))
   end
 end
