a3fb4bb0e257c108f0e57e9f0e1579b187bf2118
Undo1 <parker@erwaysoftware.com>
2015-09-18 21:04:08 -0600

    100 items per page
    http://chat.stackexchange.com/transcript/message/24199164#24199164

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,7 +9,7 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.all.includes(:reasons).includes(:feedbacks).paginate(:page => params[:page], :per_page => 50).order('created_at DESC')
+    @posts = Post.all.includes(:reasons).includes(:feedbacks).paginate(:page => params[:page], :per_page => 100).order('created_at DESC')
     @sites = Site.where(:id => @posts.map(&:site_id))
   end
 
--- app/controllers/reasons_controller.rb
+++ app/controllers/reasons_controller.rb
@@ -1,7 +1,7 @@
 class ReasonsController < ApplicationController
   def show
     @reason = Reason.find(params[:id])
-    @posts = @reason.posts.includes(:reasons).includes(:feedbacks).paginate(:page => params[:page], :per_page => 10).order('created_at DESC')
+    @posts = @reason.posts.includes(:reasons).includes(:feedbacks).paginate(:page => params[:page], :per_page => 100).order('created_at DESC')
     @sites = Site.where(:id => @posts.map(&:site_id))
   end
 end
