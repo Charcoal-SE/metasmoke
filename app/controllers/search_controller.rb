6d764d3d81cc488db11e0f7807a5f6c38feded34
Undo1 <parker@erwaysoftware.com>
2015-11-28 21:43:02 -0700

    Ordering

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,7 +4,7 @@ class SearchController < ApplicationController
     @sites = Site.where(:id => @results.map(&:site_id))
   end
   def search_results
-    @results = Post.where("username LIKE :username AND title LIKE :title", username: "%" + params[:username] + "%", title: "%" + params[:title] + "%").paginate(:page => params[:page], :per_page => 100)
+    @results = Post.where("username LIKE :username AND title LIKE :title", username: "%" + params[:username] + "%", title: "%" + params[:title] + "%").paginate(:page => params[:page], :per_page => 100).order("created_at DESC")
 
     if params[:site].present?
       site_id = Site.find_by_site_name(params[:site])
