826101c2d32bfa3dc540a10023be558bce845598
Undo1 <parker@erwaysoftware.com>
2016-01-12 11:39:20 -0700

    Open CORS floodgates

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,13 @@ module Metasmoke
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.middleware.insert_before 0, "Rack::Cors" do
+      allow do
+        origins '*' #'http://stackoverflow.com', 'http://superuser.com', 'http://serverfault.com', /^http:\/\/.*.stackexchange.com$/, 'https://stackoverflow.com', 'https://superuser.com', 'https://serverfault.com', /^https:\/\/.*.stackexchange.com$/
+
+        resource '/posts/recent.json', :headers => :any, :methods => [:get]
+      end
+    end
   end
 end
