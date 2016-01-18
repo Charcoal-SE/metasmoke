9e02242220092644a62ab6ecd6d639247fc7c651
Undo1 <parker@erwaysoftware.com>
2016-01-17 22:41:08 -0700

    Search why data

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,7 +3,9 @@ class SearchController < ApplicationController
     username = params[:username] || ""
     title = params[:title] || ""
     body = params[:body] || ""
-    @results = Post.where("username LIKE :username AND title LIKE :title AND body LIKE :body", username: "%" + username + "%", title: "%" + title + "%", body: "%" + body + "%").paginate(:page => params[:page], :per_page => 100).order("created_at DESC")
+    why = params[:why] || ""
+
+    @results = Post.where("IFNULL(username, '') LIKE :username AND IFNULL(title, '') LIKE :title AND IFNULL(body, '') LIKE :body AND IFNULL(why, '') like :why", username: "%" + username + "%", title: "%" + title + "%", body: "%" + body + "%", why: "%" + why + "%").paginate(:page => params[:page], :per_page => 100).order("created_at DESC")
 
     if params[:site].present?
       site_id = Site.find_by_site_name(params[:site])
