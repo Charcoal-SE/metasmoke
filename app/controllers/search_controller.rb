8550bfb0f61f08968b153e39865a6489043e355d
Undo1 <parker@erwaysoftware.com>
2016-01-17 23:01:52 -0700

    Optimize search page

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -5,7 +5,7 @@ class SearchController < ApplicationController
     body = params[:body] || ""
     why = params[:why] || ""
 
-    @results = Post.where("IFNULL(username, '') LIKE :username AND IFNULL(title, '') LIKE :title AND IFNULL(body, '') LIKE :body AND IFNULL(why, '') like :why", username: "%" + username + "%", title: "%" + title + "%", body: "%" + body + "%", why: "%" + why + "%").paginate(:page => params[:page], :per_page => 100).order("created_at DESC")
+    @results = Post.where("IFNULL(username, '') LIKE :username AND IFNULL(title, '') LIKE :title AND IFNULL(body, '') LIKE :body AND IFNULL(why, '') like :why", username: "%" + username + "%", title: "%" + title + "%", body: "%" + body + "%", why: "%" + why + "%").paginate(:page => params[:page], :per_page => 100).order("created_at DESC").includes(:reasons).includes(:feedbacks)
 
     if params[:site].present?
       site_id = Site.find_by_site_name(params[:site])
