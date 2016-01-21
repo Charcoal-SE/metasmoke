073b4ea251919927500c1c7168a25d0bb2877039
Undo1 <Undo1@users.noreply.github.com>
2016-01-20 22:39:00 -0700

    Include 'true' and 'false' in statistics
    http://chat.stackexchange.com/transcript/message/26994116#26994116

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -2,9 +2,9 @@ class Feedback < ActiveRecord::Base
   belongs_to :post
 
   def is_positive?
-    self.feedback_type.include? "tp"
+    self.feedback_type.include? "t"
   end
   def is_negative?
-    self.feedback_type.include? "fp"
+    self.feedback_type.include? "f"
   end
 end
