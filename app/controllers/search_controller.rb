b258bf0b81a9ecdb98298ed90ce2563a425e1275
Undo1 <parker@erwaysoftware.com>
2016-01-26 18:58:55 -0700

    Allow searching for feedback

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -5,7 +5,22 @@ class SearchController < ApplicationController
     body = params[:body] || ""
     why = params[:why] || ""
 
-    @results = Post.where("IFNULL(username, '') LIKE :username AND IFNULL(title, '') LIKE :title AND IFNULL(body, '') LIKE :body AND IFNULL(why, '') like :why", username: "%" + username + "%", title: "%" + title + "%", body: "%" + body + "%", why: "%" + why + "%").paginate(:page => params[:page], :per_page => 100).order("created_at DESC").includes(:reasons).includes(:feedbacks)
+    case params[:feedback]
+      when /true/
+        feedback = "t"
+      when /false/
+        feedback = "f"
+    end
+
+    @results = Post.where("IFNULL(username, '') LIKE :username AND IFNULL(title, '') LIKE :title AND IFNULL(body, '') LIKE :body AND IFNULL(why, '') LIKE :why", username: "%" + username + "%", title: "%" + title + "%", body: "%" + body + "%", why: "%" + why + "%")
+                   .paginate(:page => params[:page], :per_page => 100)
+                   .order("created_at DESC")
+                   .includes(:reasons)
+                   .includes(:feedbacks)
+
+    if feedback.present?
+      @results = @results.joins(:feedbacks).where("feedbacks.feedback_type LIKE :feedback", feedback: "%" + feedback + "%")
+    end
 
     if params[:site].present?
       site_id = Site.find_by_site_name(params[:site])
