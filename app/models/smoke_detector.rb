1d9a4ea6184d69bef9d27168c7f5a1c1b99c93af
Undo1 <parker@erwaysoftware.com>
2016-02-20 18:20:40 -0700

    Color coding and relative dates

--- app/models/smoke_detector.rb
+++ app/models/smoke_detector.rb
@@ -1,2 +1,15 @@
 class SmokeDetector < ActiveRecord::Base
+  def self.status_color
+    SmokeDetector.select("last_ping").order("last_ping DESC").first.status_color
+  end
+
+  def status_color
+    if last_ping > 90.seconds.ago
+      return "good"
+    elsif last_ping >= 3.minutes.ago
+      return "warning"
+    elsif last_ping < 3.minutes.ago
+      return "critical"
+    end
+  end
 end
