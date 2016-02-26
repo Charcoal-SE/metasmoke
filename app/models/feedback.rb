8cca7df6f73297a020346102bc4d7880483e6a1f
Undo1 <parker@erwaysoftware.com>
2016-02-25 21:02:35 -0700

    Cache tp and fp on posts. Will make querying these a little easier

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -1,10 +1,25 @@
 class Feedback < ActiveRecord::Base
   belongs_to :post
+  before_save :update_post_feedback_cache
 
   def is_positive?
     self.feedback_type.include? "t"
   end
   def is_negative?
     self.feedback_type.include? "f"
+  end
+
+  def update_post_feedback_cache
+    return unless self.changed?
+
+    if self.is_negative?
+      post = self.post
+      post.is_fp = true
+      post.save! if post.changed?
+    elsif self.is_positive?
+      post = self.post
+      post.is_tp = true
+      post.save! if post.changed?
+    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160223153955) do
+ActiveRecord::Schema.define(version: 20160226035019) do
 
   create_table "feedbacks", force: :cascade do |t|
     t.string  "message_link",  limit: 255
@@ -40,6 +40,8 @@ ActiveRecord::Schema.define(version: 20160223153955) do
     t.integer  "upvote_count",           limit: 4
     t.integer  "downvote_count",         limit: 4
     t.integer  "stack_exchange_user_id", limit: 4
+    t.boolean  "is_tp",                                default: false
+    t.boolean  "is_fp",                                default: false
   end
 
   create_table "posts_reasons", id: false, force: :cascade do |t|
