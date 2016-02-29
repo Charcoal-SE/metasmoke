94b5d7670c0058768a9543a7ed364d4f5fa6b3ed
Undo1 <parker@erwaysoftware.com>
2016-02-29 08:34:08 -0700

    Migrate missed users

---
+++ db/migrate/20160229151218_move_missed_users_to_stack_exchange_users.rb
@@ -0,0 +1,25 @@
+class MoveMissedUsersToStackExchangeUsers < ActiveRecord::Migration
+  def change
+    Post.where("stack_exchange_user_id IS NULL").each do |post|
+      next if post.user_link.nil?
+
+      user_id = nil      
+
+      begin
+        user_id = post.user_link.scan(/\/u\/(\d*)/).first.first
+      rescue
+        next
+      end
+
+      hash = {:site_id => post.site_id, :user_id => user_id}
+      se_user = StackExchangeUser.find_or_create_by(hash)
+      se_user.reputation = post.user_reputation
+      se_user.username = post.username
+
+      post.stack_exchange_user = se_user
+
+      se_user.save!
+      post.save!
+    end
+  end
+end
