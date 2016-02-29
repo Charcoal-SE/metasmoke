d545302461420c9fbc35577e3aeeacf5cf8663c4
Undo1 <parker@erwaysoftware.com>
2016-02-29 08:32:19 -0700

    Order /users page right

--- app/controllers/stack_exchange_users_controller.rb
+++ app/controllers/stack_exchange_users_controller.rb
@@ -1,5 +1,5 @@
 class StackExchangeUsersController < ApplicationController
   def index
-    @users = StackExchangeUser.joins(:feedbacks).where("still_alive = true").where("stack_exchange_users.site_id = 1").where("feedbacks.feedback_type LIKE '%t%'").includes(:site).group(:user_id).last(100)
+    @users = StackExchangeUser.joins(:feedbacks).where("still_alive = true").where("stack_exchange_users.site_id = 1").where("feedbacks.feedback_type LIKE '%t%'").includes(:site).group(:user_id).order("created_at DESC").first(100)
   end
 end
