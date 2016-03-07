1ef7f47dc6af2d0c7dc7a59c95cd0353c95bc399
Undo1 <parker@erwaysoftware.com>
2016-03-06 19:37:04 -0700

    All time or last month for graphs

--- app/controllers/graphs_controller.rb
+++ app/controllers/graphs_controller.rb
@@ -1,4 +1,9 @@
 class GraphsController < ApplicationController
   def index
+    if params[:timeframe] == "all"
+      @posts = Post.all
+    else
+      @posts = Post.where("created_at >= ?", 1.month.ago)
+    end
   end
 end
