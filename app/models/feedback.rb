a82a8b3757435d148d0a169b7d4d3a38ac101136
Undo1 <parker@erwaysoftware.com>
2016-03-31 08:25:22 -0600

    Show symbol for NAA

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -9,6 +9,10 @@ class Feedback < ActiveRecord::Base
   def is_negative?
     self.feedback_type.include? "f"
   end
+  def is_naa?
+    self.feedback_type.include? "naa"
+  end
+
 
   def update_post_feedback_cache
     return unless self.changed?
@@ -21,6 +25,33 @@ class Feedback < ActiveRecord::Base
       post = self.post
       post.is_tp = true
       post.save! if post.changed?
+    end
+  end
+
+  # This is a really ugly way to do this, but it's fast and slightly
+  # less ugly than the alternatives I can come up with
+
+  def element_class
+    case
+      when self.is_negative?
+        "text-danger"
+      when self.is_positive?
+        "text-success"
+      else
+        ""
+    end
+  end
+
+  def element_symbol
+    case
+      when self.is_negative?
+        "&#x2717;"
+      when self.is_positive?
+        "&#x2713;"
+      when self.is_naa?
+        "&#128169;"
+      else
+        ""
     end
   end
 end
