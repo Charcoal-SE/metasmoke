6b18a725eba77fcab8f815eea82b448ae91f005b
Undo1 <parker@erwaysoftware.com>
2016-03-31 08:46:29 -0600

    Update sites from sites helper instead of trying to create new ones

--- app/helpers/sites_helper.rb
+++ app/helpers/sites_helper.rb
@@ -10,14 +10,12 @@ module SitesHelper
     puts sites
     if sites.count > 100 #all is well
       sites.each do |site|
-        if Site.find_by_site_url(site["site_url"])  == nil
-          s=Site.new
-          s.site_url = site["site_url"]
-          s.site_domain = URI.parse(s.site_url).host
-          s.site_logo = site["favicon_url"].gsub(/http:/, "")
-          s.site_name = site["name"]
-          s.save!
-        end
+        s = Site.find_or_create_by(:site_url => site["site_url"])
+        s.site_url = site["site_url"]
+        s.site_domain = URI.parse(s.site_url).host
+        s.site_logo = site["favicon_url"].gsub(/http:/, "")
+        s.site_name = site["name"]
+        s.save!
       end
     end
   end
