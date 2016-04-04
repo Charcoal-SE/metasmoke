0102f1d69119a26c68ce7f0d57fb4857e76df35e
Undo1 <parker@erwaysoftware.com>
2016-04-04 11:15:54 -0600

    Fix error on per-reason pages

--- app/controllers/reasons_controller.rb
+++ app/controllers/reasons_controller.rb
@@ -2,6 +2,6 @@ class ReasonsController < ApplicationController
   def show
     @reason = Reason.find(params[:id])
     @posts = @reason.posts.includes(:reasons).includes(:feedbacks).paginate(:page => params[:page], :per_page => 100).order('created_at DESC')
-    @sites = Site.where(:id => @posts.map(&:site_id))
+    @sites = Site.where(:id => @posts.map(&:site_id)).to_a
   end
 end
