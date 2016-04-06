a97c0d3ff5f5be3fbbb2e4c0d6e51ec556a99447
Undo1 <parker@erwaysoftware.com>
2016-04-05 22:19:25 -0600

    Fix another deprecation
    "ActiveRecord::Base.raise_in_transactional_callbacks=
    is deprecated, hasno effect and will be removed without
    replacement."

--- config/application.rb
+++ config/application.rb
@@ -20,9 +20,6 @@ module Metasmoke
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
-    # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
-
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*' #'http://stackoverflow.com', 'http://superuser.com', 'http://serverfault.com', /^http:\/\/.*.stackexchange.com$/, 'https://stackoverflow.com', 'https://superuser.com', 'https://serverfault.com', /^https:\/\/.*.stackexchange.com$/
