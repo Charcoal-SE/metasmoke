4162764411563e2f1d2f9c02a2b262418e63ce76
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-04-06 22:31:21 +0100

    add admin controller & index

---
+++ app/controllers/admin_controller.rb
@@ -0,0 +1,13 @@
+class AdminController < ApplicationController
+  before_filter :verify_admin
+
+  def index
+  end
+
+  private
+    def verify_admin
+      if !user_signed_in? || !current_user.is_admin
+        raise ActionController::RoutingError.new('Not Found')
+      end
+    end
+end
