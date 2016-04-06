c51dc3660c1f5245bd2701d5d6522e2a837fd357
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-04-06 22:33:09 +0100

    return if user is not admin

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -7,7 +7,7 @@ class AdminController < ApplicationController
   private
     def verify_admin
       if !user_signed_in? || !current_user.is_admin
-        raise ActionController::RoutingError.new('Not Found')
+        raise ActionController::RoutingError.new('Not Found') and return
       end
     end
 end
