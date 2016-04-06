4e5e3904b5056087335da3f355e73cfb327fee4a
Undo1 <parker@erwaysoftware.com>
2016-04-06 09:52:42 -0600

    Test feedback clearing access control

--- test/controllers/feedbacks_controller_test.rb
+++ test/controllers/feedbacks_controller_test.rb
@@ -1,5 +1,24 @@
 require 'test_helper'
 
 class FeedbacksControllerTest < ActionController::TestCase
+  include Devise::TestHelpers
 
+  test "should allow admin to clear feedback" do
+    sign_in users(:admin_user)
+
+    get :clear, params: { :id => Post.last.id }
+    assert_response :success
+  end
+
+  test "should not allow non-admins to clear feedback" do
+    sign_out :user
+    get :clear, params: { :id => Post.last.id }
+    assert_redirected_to new_user_session_url
+
+    assert_raises ActionController::RoutingError do
+      sign_in users(:approved_user)
+      get :clear, params: { :id => Post.last.id }
+      assert_response :not_found
+    end
+  end
 end
