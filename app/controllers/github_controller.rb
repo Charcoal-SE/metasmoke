209a64426d6640e921548006a3157636b0faf3bb
Undo1 <parker@erwaysoftware.com>
2016-04-05 22:16:52 -0600

    More filter -> action deprecation fixes

--- app/controllers/github_controller.rb
+++ app/controllers/github_controller.rb
@@ -1,5 +1,5 @@
 class GithubController < ApplicationController
-  skip_before_filter :verify_authenticity_token
+  skip_before_action :verify_authenticity_token
 
   def hook
     render text: "OK", status: 200
