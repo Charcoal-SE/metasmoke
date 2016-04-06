12fec1a41795d3a683f2f0edcb1e306428739cad
Undo1 <parker@erwaysoftware.com>
2016-04-05 23:03:13 -0600

    A few tests

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -4,12 +4,21 @@ class PostsControllerTest < ActionController::TestCase
   include Devise::TestHelpers
 
   def setup
-    sign_in User.first
+    @posts = Post.all
+    @single_post = Post.last
   end
 
   test "should get index" do
     get :index
     assert_response :success
     assert_not_nil assigns(:posts)
+  end
+
+  test "should show post" do
+    @posts.each do |post|
+      get :show, params: { :id => post.id }
+      assert_response :success
+      assert_not_nil assigns(:post)
+    end
   end
 end
