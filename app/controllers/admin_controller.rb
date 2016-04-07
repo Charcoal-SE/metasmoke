302f39af84e18f85982591bc043105e6ac9dd9a8
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-04-07 17:01:56 +0100

    change user_feedback logic

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -9,6 +9,11 @@ class AdminController < ApplicationController
   end
 
   def user_feedback
-    @feedbacks = Feedback.unscoped.joins('inner join posts on feedbacks.post_id = posts.id').where(:user_name => params[:user_name]).select('posts.title, feedbacks.*').order('feedbacks.id DESC')
+    begin
+      @user = User.find_by_email params[:user_name]
+      @feedbacks = Feedback.unscoped.joins('inner join posts on feedbacks.post_id = posts.id').where(:user_id => @user.id).select('posts.title, feedbacks.*').order('feedbacks.id DESC')
+    rescue ActiveRecord::RecordNotFound
+      @feedbacks = Feedback.unscoped.joins('inner join posts on feedbacks.post_id = posts.id').where(:user_name => params[:user_name]).select('posts.title, feedbacks.*').order('feedbacks.id DESC')
+    end
   end
 end
