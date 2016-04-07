5be2050c74a9fafe5cd3c1d95b7d82ee3d7bc8b8
Undo1 <parker@erwaysoftware.com>
2016-04-07 12:55:48 -0600

    Paginate user feedback page

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -12,12 +12,12 @@ class AdminController < ApplicationController
     @feedbacks = nil
     begin
       @user = User.find_by_email params[:user_name]
-      @feedbacks = Feedback.unscoped.joins('inner join posts on feedbacks.post_id = posts.id').where(:user_id => @user.id).select('posts.title, feedbacks.*').order('feedbacks.id DESC')
+      @feedbacks = Feedback.unscoped.joins('inner join posts on feedbacks.post_id = posts.id').where(:user_id => @user.id).select('posts.title, feedbacks.*').order('feedbacks.id DESC').paginate(:page => params[:page], :per_page => 100)
     rescue
     end
 
     if @feedbacks.nil?
-      @feedbacks = Feedback.unscoped.joins('inner join posts on feedbacks.post_id = posts.id').where(:user_name => params[:user_name]).select('posts.title, feedbacks.*').order('feedbacks.id DESC')
+      @feedbacks = Feedback.unscoped.joins('inner join posts on feedbacks.post_id = posts.id').where(:user_name => params[:user_name]).select('posts.title, feedbacks.*').order('feedbacks.id DESC').paginate(:page => params[:page], :per_page => 100)
     end
   end
 end
