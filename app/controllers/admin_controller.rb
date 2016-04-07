e8b0d126012a1298f1531f9fbbe16444a9a83502
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-04-07 22:08:54 +0100

    get stats from controller & update view

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -12,8 +12,12 @@ class AdminController < ApplicationController
     begin
       @user = User.find_by_email params[:user_name]
       @feedbacks = Feedback.unscoped.joins('inner join posts on feedbacks.post_id = posts.id').where(:user_id => @user.id).select('posts.title, feedbacks.*').order('feedbacks.id DESC')
+      @feedback_count = Feedback.unscoped.where(:user_id => @user.id).count
+      @invalid_count = Feedback.unscoped.where(:user_id => @user.id, :is_invalidated => true).count
     rescue ActiveRecord::RecordNotFound
       @feedbacks = Feedback.unscoped.joins('inner join posts on feedbacks.post_id = posts.id').where(:user_name => params[:user_name]).select('posts.title, feedbacks.*').order('feedbacks.id DESC')
+      @feedback_count = Feedback.unscoped.where(:user_name => params[:user_name]).count
+      @invalid_count = Feedback.unscoped.where(:user_name => params[:user_name], :is_invalidated => true)
     end
   end
 end
