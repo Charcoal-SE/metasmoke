d83c773be7f71b662d1be654d74b3cf17664dd8a
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-04-07 16:47:24 +0100

    add user_feedback admin tool

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -7,4 +7,8 @@ class AdminController < ApplicationController
   def recently_invalidated
     @feedbacks = Feedback.unscoped.joins('inner join posts on feedbacks.post_id = posts.id').where(:is_invalidated => true).select('posts.title, feedbacks.*').order('feedbacks.invalidated_at DESC')
   end
+
+  def user_feedback
+    @feedbacks = Feedback.unscoped.joins('inner join posts on feedbacks.post_id = posts.id').where(:user_name => params[:user_name]).select('posts.title, feedbacks.*').order('feedbacks.created_at DESC')
+  end
 end
