e818f0bb40987782763d203a0a5e5a28f2688e9f
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-04-10 14:16:43 +0000

    migrate

---
+++ db/migrate/20160410141615_remove_needs_admin_and_reason_from_posts.rb
@@ -0,0 +1,6 @@
+class RemoveNeedsAdminAndReasonFromPosts < ActiveRecord::Migration[5.0]
+  def change
+    remove_column :posts, :needs_admin, :string
+    remove_column :posts, :admin_reason, :string
+  end
+end
