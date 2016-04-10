d5bbccf16520c7ffdedefc5ebd1086dc4deaa2f0
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-04-10 13:53:11 +0100

    correct user selection

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -30,7 +30,7 @@ class AdminController < ApplicationController
   def flagged
     @flags = Flag.where(:is_completed => false)
     @sites = Site.all.to_a
-    @users = User.where(:id => @flags.pluck(:user_id)).map { |u| [u.id, u] }.to_h
+    @users = User.where(:id => @flags.pluck(:user_id)).to_h
   end
 
   def clear_flag
