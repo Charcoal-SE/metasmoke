1995fe1b386d327dd25b0daa321200111923a46a
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-05-03 17:42:13 +0000

    add ignored_user model

---
+++ app/models/ignored_user.rb
@@ -0,0 +1,3 @@
+class IgnoredUser < ApplicationRecord
+  belongs_to :user
+end
---
+++ db/migrate/20160503174131_create_ignored_users.rb
@@ -0,0 +1,10 @@
+class CreateIgnoredUsers < ActiveRecord::Migration[5.0]
+  def change
+    create_table :ignored_users do |t|
+      t.string :user_name
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
