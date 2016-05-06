6308bb021bb59324ff611adaa495f8f2c35c063c
Undo1 <parker@erwaysoftware.com>
2016-05-06 17:34:42 -0600

    Miniprofiler for everyone

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   before_action do
-    Rack::MiniProfiler.authorize_request if user_signed_in?
+    Rack::MiniProfiler.authorize_request
   end
 
   def check_if_smokedetector
