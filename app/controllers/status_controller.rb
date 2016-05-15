fc7a49d4c08363fa411e15d35aa04f625e7509a6
Undo1 <parker@erwaysoftware.com>
2016-05-15 14:32:44 -0600

    Render new commit status when available

--- app/controllers/status_controller.rb
+++ app/controllers/status_controller.rb
@@ -6,13 +6,22 @@ class StatusController < ApplicationController
   end
 
   def status_update
+
+    commit_update = CommitStatus.where('created_at >= ?', @smoke_detector.last_ping).last
+
     @smoke_detector.last_ping = DateTime.now
     @smoke_detector.location = params[:location]
 
     @smoke_detector.save!
 
     respond_to do |format|
-      format.json { render status: :created, :text => "OK" }
+      format.json do
+        if commit_update.present?
+          render status: 201, :json => commit_update
+        else
+          render status: 200
+        end
+      end
     end
   end
 end
