91ea16997bbf521b6e3b64bffd23021e6dcc9956
Undo1 <parker@erwaysoftware.com>
2016-05-15 13:51:36 -0600

    Get commit message

--- app/controllers/github_controller.rb
+++ app/controllers/github_controller.rb
@@ -5,7 +5,7 @@ class GithubController < ApplicationController
     status = CommitStatus.new
     status.commit_sha = params[:sha]
     status.status = params[:state]
-    status.commit_message = params[:commit][:message]
+    status.commit_message = params['commit']['message']
     status.save!
 
     render text: "OK", status: 200
