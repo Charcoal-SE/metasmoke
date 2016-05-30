51961c3dfe1b73158189e6a1b4d1a66110bf7e9d
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-05-30 16:48:07 +0100

    add default

--- db/migrate/20160530154118_add_is_code_admin_to_users.rb
+++ db/migrate/20160530154118_add_is_code_admin_to_users.rb
@@ -1,5 +1,5 @@
 class AddIsCodeAdminToUsers < ActiveRecord::Migration[5.0]
   def change
-    add_column :users, :is_code_admin, :boolean
+    add_column :users, :is_code_admin, :boolean, default: false
   end
 end
