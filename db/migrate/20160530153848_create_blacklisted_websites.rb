dc3741f1557267cddd3210b9cf41b99387062244
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-05-30 16:56:35 +0100

    add another default

--- db/migrate/20160530153848_create_blacklisted_websites.rb
+++ db/migrate/20160530153848_create_blacklisted_websites.rb
@@ -2,7 +2,7 @@ class CreateBlacklistedWebsites < ActiveRecord::Migration[5.0]
   def change
     create_table :blacklisted_websites do |t|
       t.string :host
-      t.boolean :is_active
+      t.boolean :is_active, :default => false
 
       t.timestamps
     end
