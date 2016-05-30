9bdaf823285f75a272fded3aad4242d8ba4ec6a4
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-05-30 16:50:22 +0100

    fill out actions

--- app/controllers/blacklist_controller.rb
+++ app/controllers/blacklist_controller.rb
@@ -6,14 +6,19 @@ class BlacklistController < ApplicationController
   end
 
   def add_website
-
+    @website = BlacklistedWebsite.new
   end
 
   def create_website
-
+    @website = BlacklistedWebsite.new(website_params)
+    @website.save
+    redirect_to url_for(:controller => :blacklist, :action => :index)
   end
 
   def deactivate_website
-
+    @website = BlacklistedWebsite.find params[:id]
+    @website.is_active = false
+    @website.save
+    redirect_to url_for(:controller => :blacklist, :action => :index)
   end
 end
