244d138ccacfe2c413fd2becf8e32ba49f48cfd6
Undo1 <parker@erwaysoftware.com>
2016-05-31 09:13:46 -0600

    Allow logged in people to have crazy high page limits

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -27,8 +27,10 @@ class SearchController < ApplicationController
       @results = Post.all
     end
 
+    per_page = (user_signed_in? and params[:per_page].present?) ? [params[:per_page], 10000].min : 100
+
     @results = @results.where("IFNULL(username, '') LIKE :username AND IFNULL(title, '') " + title_operation + " :title AND IFNULL(body, '') LIKE :body AND IFNULL(why, '') LIKE :why", username: "%" + username + "%", title: title, body: "%" + body + "%", why: "%" + why + "%")
-                   .paginate(:page => params[:page], :per_page => 100)
+                   .paginate(:page => params[:page], :per_page => per_page)
                    .order("`posts`.`created_at` DESC")
 
     if params[:option].nil?
