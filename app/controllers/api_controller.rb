aa7b0f171a2bef40e197c2e868c2ea9597e90ae0
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-06-01 19:13:58 +0100

    respond 403 for unauth

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -36,7 +36,7 @@ class ApiController < ApplicationController
   private
     def verify_key
       unless params[:key].present? && ApiKey.where(:key => params[:key]).count == 1
-        render :json => { :error_name => "unauthenticated", :error_code => 403, :error_message => "No key was passed or the passed key is invalid." }
+        render :status => 403, :json => { :error_name => "unauthenticated", :error_code => 403, :error_message => "No key was passed or the passed key is invalid." }
       end
     end
 
