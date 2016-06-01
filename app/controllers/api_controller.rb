c5eaa521a18a5cf6c6b7cd64326ab1e478ce0734
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-06-01 18:15:34 +0100

    add auth

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,2 +1,10 @@
 class ApiController < ApplicationController
+  before_action :verify_key
+
+  private
+    def verify_key
+      unless params[:key].present? && ApiKey.where(:key => params[:key]).count == 1
+        render :json => { :error_name => "unauthenticated", :error_code => 403, :error_message => "No key was passed or the passed key is invalid." }
+      end
+    end
 end
