7033381f5bec315768caf8a4532cc36c57a6e8b2
Undo1 <parker@erwaysoftware.com>
2016-06-09 11:08:03 -0600

    Apparently that doesn't do anything

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,6 +91,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_cable.url = "ws://ws.metasmoke.erwaysoftware.com"
   config.action_cable.allowed_request_origins = ['http://metasmoke.erwaysoftware.com', 'https://metasmoke.erwaysoftware.com']
 end
