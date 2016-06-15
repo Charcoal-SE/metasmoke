6cc10966b35466352492966db8f3d6d5e06ad41c
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-06-15 18:08:29 +0100

    default false

--- db/migrate/20160615160955_add_is_ignored_to_feedbacks.rb
+++ db/migrate/20160615160955_add_is_ignored_to_feedbacks.rb
@@ -1,5 +1,5 @@
 class AddIsIgnoredToFeedbacks < ActiveRecord::Migration[5.0]
   def change
-    add_column :feedbacks, :is_ignored, :boolean
+    add_column :feedbacks, :is_ignored, :boolean, default: false
   end
 end
