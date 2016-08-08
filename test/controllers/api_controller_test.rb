81d8c21a6ff3d8b82b4dcb409749909513a631bf
Undo1 <parker@erwaysoftware.com>
2016-08-07 18:40:44 -0600

    Fix deprecations in API con test

--- test/controllers/api_controller_test.rb
+++ test/controllers/api_controller_test.rb
@@ -5,7 +5,7 @@ class ApiControllerTest < ActionController::TestCase
 
   test "shouldn't allow unauthenticated users to write" do
     sign_out(:users)
-    put :create_feedback, :id => 23653, :type => 'tpu-', :key => api_keys(:one).key
+    put :create_feedback, params: { id: 23653, type: 'tpu-', key: api_keys(:one).key }
     json = JSON.parse(@response.body)
     assert_response(401)
     assert_equal 401, json['error_code']
@@ -14,7 +14,7 @@ class ApiControllerTest < ActionController::TestCase
 
   test "should return created and post feedback" do
     sign_in users(:admin_user)
-    put :create_feedback, :id => 23653, :type => 'tpu-', :key => api_keys(:one).key
+    put :create_feedback, params: { id: 23653, type: 'tpu-', key: api_keys(:one).key }
     assert_nothing_raised JSON::ParserError do
       JSON.parse(@response.body)
     end
@@ -25,7 +25,7 @@ class ApiControllerTest < ActionController::TestCase
     sign_in users(:admin_user)
 
     assert_difference ApiKey.find(api_keys(:one).id).feedbacks do
-      post :create_feedback, :id => 23653, :type => 'tpu-', :key => api_keys(:one).key
+      post :create_feedback, params: { id: 23653, type: 'tpu-', key: api_keys(:one).key }
       assert_nothing_raised JSON::ParserError do
         JSON.parse(@response.body)
       end
@@ -38,7 +38,7 @@ class ApiControllerTest < ActionController::TestCase
 
     assert_difference 'Feedback.count' do # delta of one
       2.times do
-        post :create_feedback, :id => 23653, :type => "tpu-", :key => api_keys(:one).key
+        post :create_feedback, params: { id: 23653, type: "tpu-", key: api_keys(:one).key }
       end
     end
   end
