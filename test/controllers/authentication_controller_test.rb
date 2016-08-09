9d0922d52f6a57c481d7aac5e2389aefe528308c
Undo1 <parker@erwaysoftware.com>
2016-08-08 22:07:48 -0600

    Remove tests for now

--- test/controllers/authentication_controller_test.rb
+++ test/controllers/authentication_controller_test.rb
@@ -1,14 +1,4 @@
 require 'test_helper'
 
 class AuthenticationControllerTest < ActionDispatch::IntegrationTest
-  test "should get status" do
-    get authentication_status_url
-    assert_response :success
-  end
-
-  test "should get redirect_target" do
-    get authentication_redirect_target_url
-    assert_response :success
-  end
-
 end
