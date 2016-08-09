c453b98f37108cfd279de4c176e2a1de84a6e7c1
hichris1234 <christopherncarmel@hotmail.com>
2016-08-09 19:48:57 -0400

    Don't post a feedback message if feedback has already been given
    Hopefully this works, my Rails/Ruby is not quite there...

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -51,7 +51,9 @@ class ApiController < ApplicationController
       if @feedback.is_positive?
         ActionCable.server.broadcast "smokedetector_messages", { blacklist: { uid: @post.stack_exchange_user.try(:user_id), site: @post.stack_exchange_user.try(:site).try(:site_url), post: @post.link } }
       end
-      ActionCable.server.broadcast "smokedetector_messages", { message: "#{@feedback.feedback_type} by #{current_user.username}" + (@post.id == Post.last.id ? "" : " on [#{@post.title}](#{@post.link})") }
+      if not Feedback.where(:post_id => @feedback.post_id).where.not(:id => @feedback.id).exists?
+        ActionCable.server.broadcast "smokedetector_messages", { message: "#{@feedback.feedback_type} by #{current_user.username}" + (@post.id == Post.last.id ? "" : " on [#{@post.title}](#{@post.link})") }
+      end
       render :json => @post.feedbacks, :status => 201
     else
       render :status => 500, :json => { :error_name => "failed", :error_code => 500, :error_message => "Feedback object failed to save." }
