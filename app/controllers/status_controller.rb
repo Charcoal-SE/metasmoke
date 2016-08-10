dcd95303a2e4b1054eab1dc289f0c0271b615625
Undo1 <parker@erwaysoftware.com>
2016-08-10 08:02:36 -0600

    Stop sending stuff to Smokey through status pings

--- app/controllers/status_controller.rb
+++ app/controllers/status_controller.rb
@@ -7,11 +7,6 @@ class StatusController < ApplicationController
   end
 
   def status_update
-
-    commit_update = CommitStatus.where('created_at >= ?', @smoke_detector.last_ping).last
-    invalidated = Feedback.unscoped.where(:is_invalidated => true).where('updated_at >= ?', @smoke_detector.last_ping)
-                    .select(:post_link, :feedback_type, :user_name, :chat_user_id)
-
     @smoke_detector.last_ping = DateTime.now
     @smoke_detector.location = params[:location]
 
@@ -19,7 +14,7 @@ class StatusController < ApplicationController
 
     respond_to do |format|
       format.json do
-        render :status => commit_update.present? ? 201 : 200, :json => { :commit_update => commit_update, :invalidated => invalidated }
+        render :status => 200, :plain => "hi there!"
       end
     end
   end
