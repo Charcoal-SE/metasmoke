b5e93bf5ad8e1d4ac4b589fbf4256628cbb85fe2
Undo1 <parker@erwaysoftware.com>
2016-08-19 19:18:08 -0600

    Fix searching for username
    Would have never caught that. Yay for tests!

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -29,7 +29,7 @@ class SearchController < ApplicationController
 
     per_page = (user_signed_in? and params[:per_page].present?) ? [params[:per_page].to_i, 10000].min : 100
 
-    @results = @results.where("IFNULL(username, '') LIKE :username AND IFNULL(title, '') " + title_operation + " :title AND IFNULL(body, '') LIKE :body AND IFNULL(why, '') LIKE :why", username: "%" + username + "%", title: title, body: "%" + body + "%", why: "%" + why + "%")
+    @results = @results.where("IFNULL(posts.username, '') LIKE :username AND IFNULL(title, '') " + title_operation + " :title AND IFNULL(body, '') LIKE :body AND IFNULL(why, '') LIKE :why", username: "%" + username + "%", title: title, body: "%" + body + "%", why: "%" + why + "%")
                    .paginate(:page => params[:page], :per_page => per_page)
                    .order("`posts`.`created_at` DESC")
 
