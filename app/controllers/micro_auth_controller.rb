e421d9b0259c753410b59680c9121ae7f656ec6a
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-08-24 14:58:25 +0100

    views and logic

--- app/controllers/micro_auth_controller.rb
+++ app/controllers/micro_auth_controller.rb
@@ -1,12 +1,13 @@
 class MicroAuthController < ApplicationController
   before_action :authenticate_user!, :except => [:token]
+  before_action :verify_key, :except => [:reject]
 
   def request
   end
 
   def authorize
-    token = ApiToken.new(:user => current_user, :api_key => params[:api_key], :code => generate_code(7), :token => generate_code(64), :expiry => 10.minutes.from_now)
-    if !token.save
+    @token = ApiToken.new(:user => current_user, :api_key => @api_key, :code => generate_code(7), :token => generate_code(64), :expiry => 10.minutes.from_now)
+    if !@token.save
       flash[:alert] = "Can't create a write token right now - ask an admin to look at the server logs."
     end
   end
@@ -16,18 +17,27 @@ class MicroAuthController < ApplicationController
 
   def token
     code = params[:code]
-    api_key = params[:api_key]
-    token = ApiToken.where(:code => code, :api_key => api_key)
+    token = ApiToken.where(:code => code, :api_key => @api_key)
     if token.any? && !token.first.expiry.past?
       render :json => { :token => token.first.token }
     else
-      render :json => { :error_name => 'token not found', :error_code => 404, :error_message => 'There was no token found matching the key and code.' }
+      render :json => { :error_name => 'token not found', :error_code => 404, :error_message => 'There was no token found matching the key and code.' }, :status => 404
     end
+  end
+
+  def invalid_key
   end
 
   private
     def generate_code(len)
       hash = Digest::SHA256.hexdigest("#{rand(0..9e9)}#{Time.now}")
       return hash[0..(len - 1)]
+    end
+
+    def verify_key
+      @api_key = ApiKey.find_by_key(params[:key])
+      unless params[:key].present? && @key.present?
+        render :invalid_key, :status => 400 and return
+      end
     end
 end
