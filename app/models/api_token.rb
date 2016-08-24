5e400cdedec8d9887f811d98aeb5fc72fa596134
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-08-24 14:59:58 +0100

    validation logic

--- app/models/api_token.rb
+++ app/models/api_token.rb
@@ -1,4 +1,7 @@
 class ApiToken < ApplicationRecord
   belongs_to :api_key
   belongs_to :user
+
+  validates :code, unique: true, length: { minimum: 7 }
+  validates :token, unique: true, length: { minimum: 48 }
 end
