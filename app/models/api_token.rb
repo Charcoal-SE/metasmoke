9e7c47914fc2c6068527c0bd75db94d0216f7644
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-08-24 18:33:06 +0100

    s/unique/uniqueness/

--- app/models/api_token.rb
+++ app/models/api_token.rb
@@ -2,6 +2,6 @@ class ApiToken < ApplicationRecord
   belongs_to :api_key
   belongs_to :user
 
-  validates :code, unique: true, length: { minimum: 7 }
-  validates :token, unique: true, length: { minimum: 48 }
+  validates :code, uniqueness: true, length: { minimum: 7 }
+  validates :token, uniqueness: true, length: { minimum: 48 }
 end
