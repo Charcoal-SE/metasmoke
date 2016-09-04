619462125afdbd7cb13cc2af847418fc8390a2ca
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-09-04 17:26:13 +0100

    API result ordering

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -5,19 +5,19 @@ class ApiController < ApplicationController
   skip_before_action :verify_authenticity_token, :only => [:create_feedback]
 
   def posts
-    @posts = Post.where(:id => params[:ids].split(";"))
+    @posts = Post.where(:id => params[:ids].split(";")).order(:id => :desc)
     results = @posts.paginate(:page => params[:page], :per_page => @pagesize)
     render :json => { :items => results, :has_more => has_more?(params[:page], results.count) }
   end
 
   def posts_by_feedback
-    @posts = Post.all.joins(:feedbacks).where(:feedbacks => { :feedback_type => params[:type] })
+    @posts = Post.all.joins(:feedbacks).where(:feedbacks => { :feedback_type => params[:type] }).order(:id => :desc)
     results = @posts.paginate(:page => params[:page], :per_page => @pagesize)
     render :json => { :items => results, :has_more => has_more?(params[:page], results.count) }
   end
 
   def posts_by_url
-    @post = Post.where(:link => params[:url])
+    @post = Post.where(:link => params[:url]).order(:id => :desc)
     render :json => @post
   end
 
@@ -32,14 +32,14 @@ class ApiController < ApplicationController
   end
 
   def reasons
-    @reasons = Reason.where(:id => params[:ids].split(";"))
+    @reasons = Reason.where(:id => params[:ids].split(";")).order(:id => :desc)
     results = @reasons.paginate(:page => params[:page], :per_page => @pagesize)
     render :json => { :items => results, :has_more => has_more?(params[:page], results.count) }
   end
 
   def reason_posts
     @reason = Reason.find params[:id]
-    results = @reason.posts.paginate(:page => params[:page], :per_page => @pagesize)
+    results = @reason.posts.order(:id => :desc).paginate(:page => params[:page], :per_page => @pagesize)
     render :json => { :items => results, :has_more => has_more?(params[:page], results.count) }
   end
 
