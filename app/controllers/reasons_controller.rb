befbf1d06f0b923c7e8902ab9e6a7f1b954b6d62
Undo1 <parker@erwaysoftware.com>
2016-09-24 21:57:49 -0600

    Revert "Possibly improve performance on reasons page"
    This reverts commit faa810489d023dd4e98aaa3783244d0c20157f77.
    
    I remember why I didn't do that now. Also, tests have to assert
    something to be useful :P

--- app/controllers/reasons_controller.rb
+++ app/controllers/reasons_controller.rb
@@ -1,7 +1,7 @@
 class ReasonsController < ApplicationController
   def show
     @reason = Reason.find(params[:id])
-    @posts = @reason.posts.select(:id, :title, :site_id).includes(:reasons, :feedbacks).includes(:feedbacks => [:user, :api_key]).paginate(:page => params[:page], :per_page => 100).order('created_at DESC')
+    @posts = @reason.posts.includes(:reasons, :feedbacks).includes(:feedbacks => [:user, :api_key]).paginate(:page => params[:page], :per_page => 100).order('created_at DESC')
     @sites = Site.where(:id => @posts.map(&:site_id)).to_a
   end
 end
--- test/controllers/reasons_controller_test.rb
+++ test/controllers/reasons_controller_test.rb
@@ -1,7 +1,4 @@
 require 'test_helper'
 
 class ReasonsControllerTest < ActionController::TestCase
-  test "should get reason page" do
-    get :show, params: { id: Reason.first }
-  end
 end
