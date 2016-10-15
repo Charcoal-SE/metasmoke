1a7b93bb1765dad54a123f9b61e36cf7339cbb3f
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-10-15 12:52:38 +0100

    add dev site cache update

--- app/controllers/developer_controller.rb
+++ app/controllers/developer_controller.rb
@@ -1,2 +1,17 @@
 class DeveloperController < ApplicationController
+  before_action :authenticate_user!
+  before_action :verify_developer
+
+  def update_sites
+    SitesHelper.updateSites
+    flash[:info] = "The sites cache is being updated. Completion estimated at ~30s."
+    redirect_to :back
+  end
+
+  private
+    def verify_developer
+      unless current_user.has_role?(:developer)
+        raise ActionController::RoutingError.new('Not Found') and return
+      end
+    end
 end
