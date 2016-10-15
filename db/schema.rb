39d8a404a4cee4e456e01fc4d8100c2b05b90266
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-10-15 12:37:17 +0100

    add developer to valid role names

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161012180740) do
+ActiveRecord::Schema.define(version: 20161015112623) do
 
   create_table "api_keys", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci" do |t|
     t.datetime "created_at",  null: false
@@ -169,14 +169,6 @@ ActiveRecord::Schema.define(version: 20161012180740) do
     t.datetime "created_at",                     null: false
     t.datetime "updated_at",                     null: false
     t.integer  "site_id"
-  end
-
-  create_table "statistics", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.integer  "smokedetector_id"
-    t.integer  "post_count"
-    t.integer  "api_req_count"
-    t.datetime "created_at",       null: false
-    t.datetime "updated_at",       null: false
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci" do |t|
