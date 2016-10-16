24681f87a884d4c544454f59d770bd5c2b95be1f
Undo1 <parker@erwaysoftware.com>
2016-10-15 23:13:14 -0600

    Revert "add developer to valid role names"
    This reverts commit 39d8a404a4cee4e456e01fc4d8100c2b05b90266 and some of
    39d8a404a4cee4e456e01fc4d8100c2b05b90266.
    
    It's not actually necessary to manually create a role; the system will
    automatically create a record for it when it sees a reference to a new
    role.

--- db/migrate/20161015112623_create_developer_role.rb
+++
@@ -1,9 +0,0 @@
-class CreateDeveloperRole < ActiveRecord::Migration[5.0]
-  def up
-    Role.create(:name => 'developer')
-  end
-
-  def down
-    Role.where(:name => 'developer').destroy_all!
-  end
-end
