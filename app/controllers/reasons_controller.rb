8154f487be4750c88d36b45a158b77ea9ba60399
Undo1 <parker@erwaysoftware.com>
2016-10-17 16:02:32 -0600

    Attempt speedup of reasons page

--- app/controllers/reasons_controller.rb
+++ app/controllers/reasons_controller.rb
@@ -1,7 +1,7 @@
 class ReasonsController < ApplicationController
   def show
     @reason = Reason.find(params[:id])
-    @posts = @reason.posts.includes(:reasons, :feedbacks).includes(:feedbacks => [:user, :api_key]).paginate(:page => params[:page], :per_page => 100).order('created_at DESC')
+    @posts = @reason.posts.select(:id, :created_at, :link, :title, :site_id, 'IF(LENGTH(body)>1,1,0) as body_exists').includes(:reasons, :feedbacks).includes(:feedbacks => [:user, :api_key]).paginate(:page => params[:page], :per_page => 100).order('created_at DESC')
     @sites = Site.where(:id => @posts.map(&:site_id)).to_a
   end
 end
