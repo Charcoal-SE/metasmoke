406c011045387310d306c8c781f6e06b99eeea8c
ArtOfCode <ArtOfCode-@users.noreply.github.com>
2016-12-12 19:25:55 +0000

    Update hair_on_fire_mailer.rb

--- app/mailers/hair_on_fire_mailer.rb
+++ app/mailers/hair_on_fire_mailer.rb
@@ -5,6 +5,6 @@ class HairOnFireMailer < ApplicationMailer
     @last_ping_date = smoke_detector.last_ping
     @location = smoke_detector.location
 
-    mail(to: "undo@erwaysoftware.com", cc: "hello@artofcode.co.uk", subject: "[SmokeDetector] status DOWN (last seen #{@last_ping_date})")
+    mail(to: "undo@erwaysoftware.com", cc: "hello@artofcode.co.uk; teward@ubuntu.com", subject: "[SmokeDetector] status DOWN (last seen #{@last_ping_date})")
   end
 end
