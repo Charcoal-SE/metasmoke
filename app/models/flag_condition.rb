311e67b98d442263ab9a896e275bf466302b4885
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-12-15 12:51:10 +0000

    validations

--- app/models/flag_condition.rb
+++ app/models/flag_condition.rb
@@ -2,4 +2,8 @@ class FlagCondition < ApplicationRecord
   belongs_to :user
   has_and_belongs_to_many :sites
   has_many :flag_logs
+
+  validates :min_weight, :numericality => { :greater_than_or_equal_to => 294 }
+  validates :max_poster_rep, :inclusion => { :in => 0..50 }
+  validates :min_reason_count, :inclusion => { :in => 3..Reason.count }
 end
