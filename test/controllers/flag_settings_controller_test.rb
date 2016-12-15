cbafc5ff264a4787deca0ecd794bdcbf5392f2f1
Undo1 <parker@erwaysoftware.com>
2016-12-14 19:13:30 -0700

    Fix tests

--- test/controllers/flag_settings_controller_test.rb
+++ test/controllers/flag_settings_controller_test.rb
@@ -1,6 +1,7 @@
 require 'test_helper'
 
 class FlagSettingsControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
   setup do
     @flag_setting = flag_settings(:one)
   end
@@ -11,11 +12,26 @@ class FlagSettingsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get new" do
-    get new_flag_setting_url
+    sign_out :user
+    assert_raise ActionController::RoutingError do
+      get new_flag_setting_path
+    end
+
+    sign_in users(:admin_user)
+    get new_flag_setting_path
+
     assert_response :success
   end
 
   test "should create flag_setting" do
+    sign_out :user
+
+    assert_raise ActionController::RoutingError do
+      post flag_settings_url, params: { flag_setting: { name: @flag_setting.name, value: @flag_setting.value } }
+    end
+
+    sign_in users(:admin_user)
+
     assert_difference('FlagSetting.count') do
       post flag_settings_url, params: { flag_setting: { name: @flag_setting.name, value: @flag_setting.value } }
     end
@@ -23,26 +39,28 @@ class FlagSettingsControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to flag_setting_url(FlagSetting.last)
   end
 
-  test "should show flag_setting" do
-    get flag_setting_url(@flag_setting)
-    assert_response :success
-  end
-
   test "should get edit" do
+    sign_out :user
+
+    assert_raise ActionController::RoutingError do
+      get edit_flag_setting_url(@flag_setting)
+    end
+    sign_in users(:admin_user)
+
     get edit_flag_setting_url(@flag_setting)
     assert_response :success
   end
 
   test "should update flag_setting" do
-    patch flag_setting_url(@flag_setting), params: { flag_setting: { name: @flag_setting.name, value: @flag_setting.value } }
-    assert_redirected_to flag_setting_url(@flag_setting)
-  end
+    sign_out :user
 
-  test "should destroy flag_setting" do
-    assert_difference('FlagSetting.count', -1) do
-      delete flag_setting_url(@flag_setting)
+    assert_raise ActionController::RoutingError do
+      patch flag_setting_url(@flag_setting), params: { flag_setting: { name: @flag_setting.name, value: @flag_setting.value } }
     end
 
-    assert_redirected_to flag_settings_url
+    sign_in users(:admin_user)
+
+    patch flag_setting_url(@flag_setting), params: { flag_setting: { name: @flag_setting.name, value: @flag_setting.value } }
+    assert_redirected_to flag_setting_url(@flag_setting)
   end
 end
