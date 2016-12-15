55b1edb63c9799c090ae5a48ba50e1559ba069b9
Undo1 <parker@erwaysoftware.com>
2016-12-15 12:36:54 -0700

    Better validation error messages

--- app/models/flag_condition.rb
+++ app/models/flag_condition.rb
@@ -4,6 +4,6 @@ class FlagCondition < ApplicationRecord
   has_many :flag_logs, :dependent => :destroy
 
   validates :min_weight, :numericality => { :greater_than_or_equal_to => 294 }
-  validates :max_poster_rep, :inclusion => { :in => 0..50 }
-  validates :min_reason_count, :inclusion => { :in => 3..25 }
+  validates :max_poster_rep, :numericality => { :greater_than_or_equal_to => 0, :less_than_or_equal_to => 50 }
+  validates :min_reason_count, :numericality => { :greater_than_or_equal_to => 3, :less_than_or_equal_to => 25 }
 end
