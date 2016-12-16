6a8278ff7808209c80d5ff48fd8aa452bc3d398e
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-12-16 17:40:59 +0000

    preferences controller

--- app/controllers/user_site_settings_controller.rb
+++ app/controllers/user_site_settings_controller.rb
@@ -1,6 +1,8 @@
 class UserSiteSettingsController < ApplicationController
   before_action :authenticate_user!
   before_action :verify_admin, :only => [:for_user]
+  before_action :set_preference, :only => [:edit, :update, :destroy]
+  before_action :verify_authorized, :only => [:edit, :update, :destroy]
 
   def index
     @preferences = UserSiteSetting.where(:user => current_user)
@@ -24,5 +26,40 @@ class UserSiteSettingsController < ApplicationController
     else
       render :new, :status => 422
     end
+  end
+
+  def edit
+  end
+
+  def update
+    @preference.sites = params[:user_site_setting][:sites].map{ |i| Site.find i.to_i if i.present? }.compact
+    if @preference.update preference_params
+      flash[:success] = "Saved your preferences."
+      redirect_to url_for(:controller => :user_site_settings, :action => :index)
+    else
+      render :edit, :status => 422
+    end
+  end
+
+  def destroy
+    if @preference.destroy
+      flash[:success] = "Removed your preferences."
+    else
+      flash[:danger] = "Failed to remove your preferences - contact a developer to find out why."
+    end
+    redirect_to url_for(:controller => :user_site_settings, :action => :index)
+  end
+
+  private
+  def set_preference
+    @preference = UserSiteSetting.find params[:id]
+  end
+
+  def preference_params
+    params.require(:user_site_setting).permit(:max_flags)
+  end
+
+  def verify_authorized
+    current_user.has_role?(:admin) || @preference.user == current_user
   end
 end
