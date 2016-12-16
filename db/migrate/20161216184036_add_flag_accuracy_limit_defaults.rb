306a5c2cb0bebbd5dc16fcd50a14585857fe6725
Undo1 <parker@erwaysoftware.com>
2016-12-16 11:55:14 -0700

    Validate based on accuracy and post count instead of simple conditions

---
+++ db/migrate/20161216184036_add_flag_accuracy_limit_defaults.rb
@@ -0,0 +1,13 @@
+class AddFlagAccuracyLimitDefaults < ActiveRecord::Migration[5.0]
+  def change
+    FlagSetting.create(
+      :name => "min_accuracy",
+      :value => "99"
+    )
+
+    FlagSetting.create(
+      :name => "min_post_count",
+      :value => "1000"
+    )
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161216181659) do
+ActiveRecord::Schema.define(version: 20161216184036) do
 
   create_table "api_keys", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci" do |t|
     t.datetime "created_at",  null: false
