78c297401f591effee3fa211ee3eedce507dc86b
Undo1 <parker@erwaysoftware.com>
2016-12-28 09:30:17 -0700

    Speed up prettier flag log view

--- app/controllers/flag_log_controller.rb
+++ app/controllers/flag_log_controller.rb
@@ -1,5 +1,6 @@
 class FlagLogController < ApplicationController
   def index
-    @flag_logs = FlagLog.all.order('created_at DESC').includes(:post, :user).paginate(:page => params[:page], :per_page => 100)
+    @flag_logs = FlagLog.all.order('created_at DESC').includes(:post => [:feedbacks => [:user, :api_key]]).includes(:user).paginate(:page => params[:page], :per_page => 100)
+    @sites = Site.where(:id => @flag_logs.map(&:post).map(&:site_id)).to_a
   end
 end
