b11602bc96883d4fe6bb7bb51e17f207221f8dba
Undo1 <parker@erwaysoftware.com>
2016-12-30 15:59:34 -0700

    Post row performance on flag log pages

--- app/controllers/flag_log_controller.rb
+++ app/controllers/flag_log_controller.rb
@@ -1,12 +1,12 @@
 class FlagLogController < ApplicationController
   def index
-    @flag_logs = FlagLog.all.order('created_at DESC').includes(:post => [:feedbacks => [:user, :api_key]]).includes(:user).paginate(:page => params[:page], :per_page => 100)
+    @flag_logs = FlagLog.all.order('created_at DESC').includes(:post => [:feedbacks => [:user, :api_key]]).includes(:post => [:reasons]).includes(:user).paginate(:page => params[:page], :per_page => 100)
     @sites = Site.where(:id => @flag_logs.map(&:post).map(&:site_id)).to_a
   end
 
   def by_post
     @individual_post = Post.find(params[:id])
-    @flag_logs = @individual_post.flag_logs.order('created_at DESC').includes(:post => [:feedbacks => [:user, :api_key]]).includes(:user).paginate(:page => params[:page], :per_page => 100)
+    @flag_logs = @individual_post.flag_logs.order('created_at DESC').includes(:post => [:feedbacks => [:user, :api_key]]).includes(:post => [:reasons]).includes(:user).paginate(:page => params[:page], :per_page => 100)
     @sites = Site.where(:id => @flag_logs.map(&:post).map(&:site_id)).to_a
     render :index
   end
