47b9ef26d0d1f9a3c9e9803c15f438fd36e62afc
ArtOfCode- <owen.jenkins2903@gmail.com>
2017-01-13 10:52:46 +0000

    fix some display issues

--- db/migrate/20170113102808_add_site_to_flag_logs.rb
+++ db/migrate/20170113102808_add_site_to_flag_logs.rb
@@ -1,5 +1,9 @@
 class AddSiteToFlagLogs < ActiveRecord::Migration[5.0]
   def change
     add_reference :flag_logs, :site, foreign_key: true
+
+    FlagLog.all.each do |fl|
+      fl.update(:site => fl.post.site)
+    end
   end
 end
