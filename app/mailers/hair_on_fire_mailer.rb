5233d260f5b2cc8c099e0966613e59db2ae2d044
Undo1 <parker@erwaysoftware.com>
2017-01-19 10:21:07 -0700

    Add Thomas to hair-on-fire mailer.
    https://chat.stackexchange.com/transcript/message/34885327#34885327

--- app/mailers/hair_on_fire_mailer.rb
+++ app/mailers/hair_on_fire_mailer.rb
@@ -5,6 +5,6 @@ class HairOnFireMailer < ApplicationMailer
     @last_ping_date = smoke_detector.last_ping
     @location = smoke_detector.location
 
-    mail(to: smoke_detector.user.email, cc: "undo@erwaysoftware.com; hello@artofcode.co.uk", subject: "[SmokeDetector] status DOWN (last seen #{@last_ping_date})")
+    mail(to: smoke_detector.user.email, cc: "teward@ubuntu.com; undo@erwaysoftware.com; hello@artofcode.co.uk", subject: "[SmokeDetector] status DOWN (last seen #{@last_ping_date})")
   end
 end
