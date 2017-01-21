18be0a2eb9b177b153b6f3c2fb66056141079487
ArtOfCode- <owen.jenkins2903@gmail.com>
2017-01-21 15:18:47 +0000

    don't allow preferences with 0 sites to save; closes #74

--- app/models/user_site_setting.rb
+++ app/models/user_site_setting.rb
@@ -1,4 +1,13 @@
 class UserSiteSetting < ApplicationRecord
   belongs_to :user
   has_and_belongs_to_many :sites
+
+  validate :site_count
+
+  private
+  def site_count
+    unless self.sites.present?
+      errors.add(:sites, "must contain at least one site")
+    end
+  end
 end
