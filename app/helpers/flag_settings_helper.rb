37f7ca3ed9734b91c8413cc81cdd156620e0a19f
Undo1 <parker@erwaysoftware.com>
2017-02-21 09:10:57 -0700

    Add FlagSetting plot lines to relevant graphs
    Closes #94

--- app/helpers/flag_settings_helper.rb
+++ app/helpers/flag_settings_helper.rb
@@ -1,2 +1,16 @@
 module FlagSettingsHelper
+  def flag_settings_plot_lines(start_time, end_time=Time.now)
+    setting_ids = FlagSetting.where(:name => ["max_flags", "min_accuracy"]).pluck(:id)
+
+    audits = Audited::Audit.where(:auditable_type => "FlagSetting")
+             .where(:auditable_id => setting_ids)
+             .where(:created_at => start_time..end_time)
+
+    audits.map do |a|
+      changes = a.audited_changes["value"]
+      {"width" => '1', :value => a.created_at.to_i * 1000, :color => 'lightgrey',
+        :label => { :text => "#{FlagSetting.find(a.auditable_id).name.humanize}: #{changes.first} -> #{changes.last}" },
+        "zIndex" => 10 }
+    end
+  end
 end
