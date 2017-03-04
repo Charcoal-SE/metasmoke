e46dda590b56f3f1e09d6b77ede0b0c326ac98f4
Jed Fox <git@twopointzero.us>
2017-03-04 10:29:28 -0500

    Call the GitHub API server-side
    Also:
    
    * Adds the current commit’s message
    * Makes the `authorized_post` method public
    * Adds an `authorized_get` method
    * Adds both of the above to ApiHelper for global usage

--- app/controllers/code_status_controller.rb
+++ app/controllers/code_status_controller.rb
@@ -1,3 +1,5 @@
+include ApiHelper
+
 class CodeStatusController < ApplicationController
   def index
     @gem_versions = Gem::Specification.sort_by{ |g| [g.name.downcase, g.version] }.sort_by(&:name)
@@ -6,5 +8,26 @@ class CodeStatusController < ApplicationController
       "will_paginate",
       "turbolinks"
     ]
+  end
+  def api
+    @repo = Rails.cache.fetch "code_status/repo##{CurrentCommit}", expires_in: 5.minutes do
+      ApiHelper.authorized_get("https://api.github.com/repos/Charcoal-SE/metasmoke")
+    end
+    @compare = Rails.cache.fetch "code_status/compare##{CurrentCommit}", expires_in: 1.minute do
+      ApiHelper.authorized_get("https://api.github.com/repos/Charcoal-SE/metasmoke/compare/#{CurrentCommit}...#{@repo[:default_branch]}")
+    end
+    @compare_diff = Rails.cache.fetch "code_status/compare_diff##{CurrentCommit}", expires_in: 1.minute do
+      ApiHelper.authorized_get("https://api.github.com/repos/Charcoal-SE/metasmoke/compare/#{CurrentCommit}...#{@repo[:default_branch]}", headers: {
+        Accept: "application/vnd.github.v3.diff"
+      }, parse_json: false)
+    end
+    @commit = Rails.cache.fetch "code_status/commit##{CurrentCommit}", expires_in: 5.minutes do
+      ApiHelper.authorized_get("https://api.github.com/repos/Charcoal-SE/metasmoke/commits/#{CurrentCommit}")
+    end
+    @commit_diff = Rails.cache.fetch "code_status/commit_diff##{CurrentCommit}", expires_in: 1.day do
+      ApiHelper.authorized_get("https://api.github.com/repos/Charcoal-SE/metasmoke/commits/#{CurrentCommit}", headers: {
+        Accept: "application/vnd.github.v3.diff"
+      }, parse_json: false)
+    end
   end
 end
