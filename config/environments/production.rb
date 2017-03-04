13058b676addbec412946d5dda5ec0ef1ddb0311
Undo1 <parker@erwaysoftware.com>
2017-03-04 10:06:42 -0700

    Stop using mem store in prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,7 @@ Rails.application.configure do
   # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
 
   # Use a different cache store in production.
-  config.cache_store = :memory_store, { size: 10.megabytes }
+  # config.cache_store = :memory_store, { size: 10.megabytes }
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
