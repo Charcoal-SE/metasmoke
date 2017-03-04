29e4cdf4ca9d62bbada0b67f87f487e7e673cc4b
Undo1 <parker@erwaysoftware.com>
2017-03-04 09:24:52 -0700

    Switch to Octokit; strip trailing newline off CurrentCommit

--- app/controllers/code_status_controller.rb
+++ app/controllers/code_status_controller.rb
@@ -11,23 +11,19 @@ class CodeStatusController < ApplicationController
   end
   def api
     @repo = Rails.cache.fetch "code_status/repo##{CurrentCommit}", expires_in: 5.minutes do
-      ApiHelper.authorized_get("https://api.github.com/repos/Charcoal-SE/metasmoke")
+      Octokit.repository "Charcoal-SE/metasmoke"
     end
     @compare = Rails.cache.fetch "code_status/compare##{CurrentCommit}", expires_in: 1.minute do
-      ApiHelper.authorized_get("https://api.github.com/repos/Charcoal-SE/metasmoke/compare/#{CurrentCommit}...#{@repo[:default_branch]}")
+      Octokit.compare "Charcoal-SE/metasmoke", CurrentCommit, @repo[:default_branch]
     end
     @compare_diff = Rails.cache.fetch "code_status/compare_diff##{CurrentCommit}", expires_in: 1.minute do
-      ApiHelper.authorized_get("https://api.github.com/repos/Charcoal-SE/metasmoke/compare/#{CurrentCommit}...#{@repo[:default_branch]}", headers: {
-        Accept: "application/vnd.github.v3.diff"
-      }, parse_json: false)
+      Octokit.compare "Charcoal-SE/metasmoke", CurrentCommit, @repo[:default_branch], accept: "application/vnd.github.v3.diff"
     end
     @commit = Rails.cache.fetch "code_status/commit##{CurrentCommit}", expires_in: 5.minutes do
-      ApiHelper.authorized_get("https://api.github.com/repos/Charcoal-SE/metasmoke/commits/#{CurrentCommit}")
+      Octokit.commit "Charcoal-SE/metasmoke", CurrentCommit
     end
     @commit_diff = Rails.cache.fetch "code_status/commit_diff##{CurrentCommit}", expires_in: 1.day do
-      ApiHelper.authorized_get("https://api.github.com/repos/Charcoal-SE/metasmoke/commits/#{CurrentCommit}", headers: {
-        Accept: "application/vnd.github.v3.diff"
-      }, parse_json: false)
+      Octokit.commit "Charcoal-SE/metasmoke", CurrentCommit, accept: "application/vnd.github.v3.diff"
     end
   end
 end
--- config/initializers/revision.rb
+++ config/initializers/revision.rb
@@ -1,5 +1,5 @@
 if File.readable?("REVISION")
-  CurrentCommit = File.read("REVISION")
+  CurrentCommit = File.read("REVISION").strip
 else
   CurrentCommit = nil
 end
