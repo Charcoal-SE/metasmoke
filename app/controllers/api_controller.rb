5be752a70921b2ca32455deb49c9dbee70d6a027
ArtOfCode <owen.jenkins2903@gmail.com>
2017-03-16 22:38:31 +0000

    Skip key verification on filter_fields

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,5 +1,5 @@
 class ApiController < ApplicationController
-  before_action :verify_key, :except => [:filter_generator, :api_docs]
+  before_action :verify_key, :except => [:filter_generator, :api_docs, :filter_fields]
   before_action :set_pagesize, :except => [:filter_generator, :api_docs]
   before_action :verify_write_token, :only => [:create_feedback, :report_post, :spam_flag]
   skip_before_action :verify_authenticity_token, :only => [:posts_by_url, :create_feedback, :report_post, :spam_flag]
