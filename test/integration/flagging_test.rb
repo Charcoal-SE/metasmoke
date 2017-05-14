015fad7b53fcd298fc3cbf0f44633f3a050645b9
Undo1 <parker@erwaysoftware.com>
2017-05-13 22:23:58 -0600

    Test revision fetching

--- test/integration/flagging_test.rb
+++ test/integration/flagging_test.rb
@@ -17,11 +17,47 @@ class FlaggingTest < ActionDispatch::IntegrationTest
     site_setting.sites = Site.mains
     site_setting.save!
 
-    # Webmock setup
+    # Pick a couple of random main sites
 
-    webmock_file = "#{Rails.root}/test/integration/webmock_json_responses/mod_sites_response.json"
+    @site = Site.mains.where(max_flags_per_post: 3).order("RAND()").last
+    @limited_site = Site.mains.where(max_flags_per_post: 1).order("RAND()").last
+
+    # Post setup
+
+    @stack_id = 1234
+    @multi_rev_stack_id = 4321
+
+    @post = Post.new({
+      link: "//#{@site.site_domain}/questions/#{@stack_id}",
+      site: @site
+    })
+
+    @multi_rev_post = Post.new({
+      link: "//#{@site.site_domain}/questions/#{@multi_rev_stack_id}",
+      site: @site
+    })
+
+    @limited_post = Post.new({
+      link: "//#{@limited_site.site_domain}/questions/#{@stack_id}",
+      site: @limited_site
+    })
+
+    setup_webmock
+  end
+
+  def setup_webmock
     stub_request(:get, /https:\/\/api.stackexchange.com\/2\.2\/me\/associated/).
-      to_return(:status => 200, :body => File.open(webmock_file).read(), :headers => {})
+      to_return(:status => 200, :body => webmock_file("mod_sites_response"), :headers => {})
+
+    @single_rev_stub = stub_request(:get, /https:\/\/api.stackexchange.com\/2\.2\/posts\/#{@stack_id}\/revisions/).
+      to_return(status: 200, body: webmock_file("single_revision_response"), headers: {})
+
+    @multi_rev_stub = stub_request(:get, /https:\/\/api.stackexchange.com\/2\.2\/posts\/#{@multi_rev_stack_id}\/revisions/).
+      to_return(status: 200, body: webmock_file("multi_revision_response"), headers: {})
+  end
+
+  def webmock_file(name)
+    File.open("#{Rails.root}/test/integration/webmock_json_responses/#{name}.json").read()
   end
 
   test "should update moderator sites" do
@@ -50,5 +86,17 @@ class FlaggingTest < ActionDispatch::IntegrationTest
     assert_raise do
       @user.spam_flag(Post.new)
     end
+  end
+
+  test "should set revision count" do
+    @post.get_revision_count
+
+    assert_requested @single_rev_stub
+    assert_equal @post.revision_count, 1
+
+    @multi_rev_post.get_revision_count
+
+    assert_requested @multi_rev_stub
+    assert_operator @multi_rev_post.revision_count, :>, 1
   end
 end
