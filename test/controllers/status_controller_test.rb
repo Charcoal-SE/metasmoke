c5282d2d2ac1cdf447a15f5a445f342a048ea69d
Undo1 <parker@erwaysoftware.com>
2017-05-15 21:03:47 -0600

    More coverage in status controller tests

--- test/controllers/status_controller_test.rb
+++ test/controllers/status_controller_test.rb
@@ -1,17 +1,28 @@
 require 'test_helper'
 
 class StatusControllerTest < ActionController::TestCase
+  def setup
+    @smoke_detector = SmokeDetector.order(:last_ping).last
+  end
+
   test "should get index" do
     [0, 2, 10].each do |m|
-      SmokeDetector.order(:last_ping).last.update(:last_ping => m.minutes.ago)
+      @smoke_detector.update(:last_ping => m.minutes.ago)
       get :index
       assert_response :success
     end
   end
 
   test "should update last-seen time on status ping" do
-    old_ping_time = SmokeDetector.last.last_ping
-    post :status_update, params: { :key => SmokeDetector.last.access_token, :location => SmokeDetector.last.location}
-    assert_in_delta Time.now.to_i, SmokeDetector.last.last_ping.to_i, 2
+    old_ping_time = @smoke_detector.last_ping
+    post :status_update, params: { :key => @smoke_detector.access_token, :location => @smoke_detector.location}
+    assert_in_delta Time.now.to_i, @smoke_detector.reload.last_ping.to_i, 2
+  end
+
+  test "should failover" do
+    SmokeDetector.update_all(is_standby: true)
+
+    post :status_update, params: { :key => @smoke_detector.access_token, :location => @smoke_detector.location}
+    assert_equal false, @smoke_detector.is_standby
   end
 end
