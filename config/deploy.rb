69973f42e52937aea5fb1fbe721c2199f2bac50c
Undo1 <parker@erwaysoftware.com>
2017-06-09 16:07:45 -0600

    Keep logs in one place

--- config/deploy.rb
+++ config/deploy.rb
@@ -30,14 +30,9 @@ set :linked_files, fetch(:linked_files, ['config/database.yml']).push(
 )
 
 # Default value for linked_dirs is []
-# set :linked_dirs, fetch(:linked_dirs, []).push(
-#   'log',
-#   'tmp/pids',
-#   'tmp/cache',
-#   'tmp/sockets',
-#   'vendor/bundle',
-#   'public/system'
-# )
+set :linked_dirs, fetch(:linked_dirs, []).push(
+  'log'
+)
 
 # Default value for default_env is {}
 # set :default_env, { path: '/opt/ruby/bin:$PATH' }
