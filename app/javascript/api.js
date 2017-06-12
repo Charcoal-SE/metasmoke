58a0466b9618369c1dff324badd96918ffd0adc4
Michał Perłakowski <michal@perlakowski.pl>
2017-06-12 23:01:44 +0200

    Refactor to more functional style

--- app/javascript/api.js
+++ app/javascript/api.js
@@ -6,27 +6,22 @@ const debug = createDebug('ms:api');
 
 onLoad(() => {
   $('#create_filter').on('click', () => {
-    const checkboxes = $('input[type=checkbox]');
-    const bits = new Array(checkboxes.length);
-
-    $.each(checkboxes, (index, item) => {
-      const $item = $(item);
-      const arrayIndex = $item.data('index');
-      if ($item.is(':checked')) {
-        bits[arrayIndex] = 1;
+    const bits = Array.from($('input[type=checkbox]')).reduce((acc, x) => {
+      acc[$(x).data('index')] = $(x).is(':checked') ? 1 : 0;
+      return acc;
+    }, []);
+    const last = arr => arr[arr.length - 1];
+    const chunk = n => arr => arr.reduce((acc, x) => {
+      if (acc.length !== 0 && last(acc).length < n) {
+        last(acc).push(x);
       } else {
-        bits[arrayIndex] = 0;
+        acc.push([x]);
       }
-    });
-
-    let unsafeFilter = '';
-    while (bits.length) {
-      const nextByte = bits.splice(0, 8).join('');
-      const charCode = parseInt(nextByte.toString(), 2);
-      unsafeFilter += String.fromCharCode(charCode);
-      debug({ nextByte, charCode, unsafeFilter });
-    }
-
+      return acc;
+    }, []);
+    const glue = x => x.join('');
+    const toChar = x => String.fromCharCode(parseInt(x, 2));
+    const unsafeFilter = glue(chunk(8)(bits).map(glue).map(toChar));
     const filter = encodeURIComponent(unsafeFilter);
     window.prompt('Calculated, URL-encoded filter:', filter);
   });
