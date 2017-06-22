355e03771dda98b38e622ddbf6a992461674633f
NobodyNada <jkeller234@icloud.com>
2017-06-22 10:30:22 -0700

    Fix commit message when merging blacklist PRs

--- app/controllers/github_controller.rb
+++ app/controllers/github_controller.rb
@@ -213,7 +213,7 @@ class GithubController < ApplicationController
         Octokit.client.merge_pull_request(
           'Charcoal-SE/SmokeDetector',
           pr_num,
-          'Merge blacklist request ##{pr_num} --autopull'
+          '--autopull'
         )
         message = "Merged SmokeDetector [##{pr_num}](https://github.com/Charcoal-SE/SmokeDetector/pull/#{pr_num})."
         ActionCable.server.broadcast('smokedetector_messages', message: message)
