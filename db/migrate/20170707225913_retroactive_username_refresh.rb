62f702bde2484a74e03236a2120dccf653870578
Undo1 <parker@erwaysoftware.com>
2017-07-13 19:51:33 -0600

    Appease rubocop

--- db/migrate/20170707225913_retroactive_username_refresh.rb
+++ db/migrate/20170707225913_retroactive_username_refresh.rb
@@ -1,8 +1,9 @@
+# frozen_string_literal: true
+
 class RetroactiveUsernameRefresh < ActiveRecord::Migration[5.1]
   def up
-    User.where(username: ["1", "", nil]).each(&:get_username)
+    User.where(username: ['1', '', nil]).each(&:get_username)
   end
 
-  def down
-  end
+  def down; end
 end
