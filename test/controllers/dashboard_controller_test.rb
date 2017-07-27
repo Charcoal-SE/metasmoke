6335bb6b5dfb7419279e4dd082db3441a6d6b107
ArtOfCode- <hello@artofcode.co.uk>
2017-07-27 01:00:41 +0100

    Feedback, round 1

--- test/controllers/dashboard_controller_test.rb
+++ test/controllers/dashboard_controller_test.rb
@@ -3,18 +3,18 @@
 require 'test_helper'
 
 class DashboardControllerTest < ActionController::TestCase
-  test "should work for anonymous users" do
+  test 'should work for anonymous users' do
     get :new_dash
     assert_response :success
   end
 
-  test "should work for flaggers" do
+  test 'should work for flaggers' do
     sign_in users(:unapproved_user)
     get :new_dash
     assert_response :success
   end
 
-  test "should work for reviewers" do
+  test 'should work for reviewers' do
     sign_in users(:approved_user)
     get :new_dash
     assert_response :success
