5638573e3499a2c79c86d747ae7714e7028a7f23
ArtOfCode- <hello@artofcode.co.uk>
2017-07-31 23:24:22 +0100

    Ruboc-- wait, no, NPM

--- app/javascript/cable/status.js
+++ app/javascript/cable/status.js
@@ -4,22 +4,23 @@ import cable from './cable';
 
 const debug = createDebug('ms:status');
 
+let statusSocket;
 route('/status', () => {
-  let statusSocket = cable.subscriptions.create('StatusChannel', {
+  statusSocket = cable.subscriptions.create('StatusChannel', {
     received(data) {
       debug('received', data);
 
-      let {id, ts_unix, ts_ago, ts_raw, location} = data;
-      if ([id, ts_unix, ts_ago, ts_raw, location].some(x => x === null || x === undefined)) {
+      const { id, tsUnix, tsAgo, tsRaw, location } = data;
+      if ([id, tsUnix, tsAgo, tsRaw, location].some(x => x === null || x === undefined)) {
         return;
       }
 
-      let row = $(`.status-row[data-id=${id}]`);
+      const row = $(`.status-row[data-id=${id}]`);
       row.find('.location-cell').find('.location').text(location);
 
-      let pingCell = row.find('.ping-cell');
-      pingCell.attr('data-livestamp', ts_unix).attr('title', ts_raw);
-      pingCell.text(`${ts_ago} ago`);
+      const pingCell = row.find('.ping-cell');
+      pingCell.attr('data-livestamp', tsUnix).attr('title', tsRaw);
+      pingCell.text(`${tsAgo} ago`);
     }
   });
 }, () => {
