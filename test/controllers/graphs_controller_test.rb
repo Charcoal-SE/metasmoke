37b78690afd08e69c77ab1d6a94b0605a5e4e3e1
ArtOfCode- <hello@artofcode.co.uk>
2017-08-04 20:38:56 +0100

    Some tests

--- test/controllers/graphs_controller_test.rb
+++ test/controllers/graphs_controller_test.rb
@@ -14,6 +14,12 @@ class GraphsControllerTest < ActionController::TestCase
     assert_response 200
   end
 
+  test 'should get reports by hour cached' do
+    get :reports_by_hour, params: { cache: true }
+    JSON.parse(response.body)
+    assert_response 200
+  end
+
   test 'should get reports by site' do
     get :reports_by_site, params: { timeframe: 'all' }
     JSON.parse(response.body)
@@ -32,14 +38,56 @@ class GraphsControllerTest < ActionController::TestCase
     assert_response 200
   end
 
+  test 'should get time to deletion cached' do
+    get :time_to_deletion, params: { cache: true }
+    JSON.parse(response.body)
+    assert_response 200
+  end
+
   test 'should get flagging results' do
     get :flagging_results
     JSON.parse(response.body)
     assert_response 200
   end
 
+  test 'should get flagging results cached' do
+    get :flagging_results, params: { cache: true }
+    JSON.parse(response.body)
+    assert_response 200
+  end
+
   test 'should get flagging timeline' do
     get :flagging_timeline
+    JSON.parse(response.body)
+    assert_response 200
+  end
+
+  test 'should get flagging timeline cached' do
+    get :flagging_timeline, params: { cache: true }
+    JSON.parse(response.body)
+    assert_response 200
+  end
+
+  test 'should get monthly TTD' do
+    get :monthly_ttd
+    JSON.parse(response.body)
+    assert_response 200
+  end
+
+  test 'should get monthly TTD cached' do
+    get :monthly_ttd, params: { cache: true }
+    JSON.parse(response.body)
+    assert_response 200
+  end
+
+  test 'should get monthly TTD for 1 month' do
+    get :monthly_ttd, params: { months: 1 }
+    JSON.parse(response.body)
+    assert_response 200
+  end
+
+  test 'should get reports' do
+    get :reports
     JSON.parse(response.body)
     assert_response 200
   end
