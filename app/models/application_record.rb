c31446b9d111f140c3125115c9383f77fd8f4d7b
ArtOfCode- <hello@artofcode.co.uk>
2017-09-07 00:07:00 +0100

    mass_habtm ignores existing entries

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -17,11 +17,18 @@ class ApplicationRecord < ActiveRecord::Base
   end
 
   def self.mass_habtm(join_table, first_type, second_type, record_pairs)
-    record_ids = record_pairs.map { |p| p[0..1].map(&:id) }
+    first_ids = record_pairs.map { |p| p[0].id }.join(', ')
+    second_ids = record_pairs.map { |p| p[1].id }.join(', ')
+    pre_existing = connection.execute("SELECT #{first_type}_id, #{second_type}_id FROM #{join_table} " +
+                                      "WHERE #{first_type}_id IN (#{first_ids}) AND #{second_type}_id IN (#{second_ids})").to_a
+
+    record_ids = record_pairs.map do |pair|
+      pair_ids = [pair[0].id, pair[1].id]
+      pre_existing.include?(pair_ids) ? nil : pair_ids
+    end.compact
+
     values = record_ids.map { |p| "(#{p[0]}, #{p[1]})" }.join(', ')
     query = "INSERT INTO #{join_table} (#{first_type}_id, #{second_type}_id) VALUES #{values};"
-    transaction do
-      connection.execute query
-    end
+    connection.execute query
   end
 end
