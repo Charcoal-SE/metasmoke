11d701c862cbac49b6ffe737e991dac504f575aa
ArtOfCode- <hello@artofcode.co.uk>
2017-12-28 01:02:57 +0000

    Fix docs HTML output

--- app/api/scripts/create_docs.rb
+++ app/api/scripts/create_docs.rb
@@ -5,7 +5,7 @@ require 'redcarpet'
 renderer = Redcarpet::Render::HTML.new hard_wrap: true
 parser = Redcarpet::Markdown.new renderer
 
-output = %w[<table> <tbody> <thead> <tr><td><strong>Path</strong></td><td><strong>Description</strong></td></tr>]
+output = %w[<table> <thead> <tr><td><strong>Path</strong></td><td><strong>Description</strong></td></tr> </thead> <tbody>]
 Dir.glob('../docs/**/*.md').sort_by { |x| File.basename x }.each do |f|
   text = File.read f
   title = Regexp.new(/^# ?([\w ]+)$/).match(text)[1]
