f52b8c51e54f41e1a156b79226ec18c7704d0297
ArtOfCode- <hello@artofcode.co.uk>
2018-01-12 10:12:10 +0000

    Re-tune redaction and dump scripts

--- dump/dump.sh
+++ dump/dump.sh
@@ -8,7 +8,7 @@ echo "CREATE DATABASE dump_metasmoke" | mysql -u root
 mysqldump -u root metasmoke | mysql -u root dump_metasmoke;
 mysql -u root dump_metasmoke < $METASMOKE_ROOT/current/dump/redact.sql
 mysqldump -u root dump_metasmoke | gzip > $METASMOKE_ROOT/shared/dumps/$filename
-echo "DROP DATABASE dump_metasmoke" | mysql -u root
+echo "DROP DATABASE dump_metasmoke;" | mysql -u root
 
 # Remove old dumps
 ls -d $METASMOKE_ROOT/shared/dumps/* | grep -v $filename | xargs rm
--- dump/redact.sql
+++ dump/redact.sql
@@ -3,10 +3,9 @@ TRUNCATE TABLE `api_tokens`;
 UPDATE `api_keys` SET `key` = '';
 UPDATE `audits` SET `remote_address` = '';
 TRUNCATE TABLE `flags`;
-TRUNCATE TABLE `ignored_users`;
 UPDATE `smoke_detectors` SET `access_token` = '';
 UPDATE `users` SET
-    `email` = CONCAT(`id`, "@metasmoke.fake"),
+    `email` = CONCAT(`id`, '@metasmoke.fake'),
     `encrypted_password` = '',
     `reset_password_token` = '',
     `reset_password_sent_at` = NULL,
