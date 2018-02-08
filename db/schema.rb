061f590eae2444a2b9c04099a122b8fddf3f2004
ArtOfCode- <hello@artofcode.co.uk>
2018-02-08 18:01:23 +0000

    Account requirement

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_02_08_142918) do
+ActiveRecord::Schema.define(version: 2018_02_08_174423) do
 
   create_table "announcements", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
     t.text "text"
