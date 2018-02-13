46d925d70ed91e8ba9902372e18ea513ce78cf1b
ArtOfCode- <hello@artofcode.co.uk>
2018-02-13 18:53:05 +0000

    Alert on condition disable; closes #307

--- app/models/flag_condition.rb
+++ app/models/flag_condition.rb
@@ -29,8 +29,12 @@ class FlagCondition < ApplicationRecord
   def self.revalidate_all
     FlagCondition.where(flags_enabled: true).find_each do |fc|
       unless fc.validate
+        failures = fc.errors.full_messages
         fc.flags_enabled = false
-        fc.save!
+        fc.save(validate: false)
+        ActionCable.server.broadcast 'smokedetector_messages',
+                                     { message: "@#{fc.user&.username.tr(' ', '')} " +
+                                                "Your flag condition was disabled: #{failures.join(',')}" }
       end
     end
   end
