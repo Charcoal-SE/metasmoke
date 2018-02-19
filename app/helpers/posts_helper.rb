23f92eed2918559fafcadc03bbe193f44108d7ee
izwick-schachter <isaiah@developingtechnician.com>
2018-02-18 23:15:00 -0500

    Close #315

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
 module PostsHelper
+  def render_links(text)
+    raw(reason.split(%r{((?:https?:)?\/{2}[^\)\s]*)}).map.with_index do |s, i|
+      i % 2 == 0 ? s : link_to(s, s)
+    end.join)
+  end
 end
