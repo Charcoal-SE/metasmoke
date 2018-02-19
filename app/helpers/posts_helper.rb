8be2b05b6f9c4aebeb3968087787fe32c6429639
izwick-schachter <isaiah@developingtechnician.com>
2018-02-18 23:28:11 -0500

    Fixed sanitization

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -3,7 +3,7 @@
 module PostsHelper
   def render_links(text)
     raw(text.split(%r{((?:https?:)?\/{2}[^\)\s]*)}).map.with_index do |s, i|
-      i % 2 == 0 ? s : link_to(s, s)
+      i % 2 == 0 ? sanitize(s) : link_to(s, s)
     end.join)
   end
 end
