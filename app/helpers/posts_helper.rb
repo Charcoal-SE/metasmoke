21ab03db8509470960d2ba202e71cbaa155ee6b6
izwick-schachter <isaiah@developingtechnician.com>
2018-02-19 11:22:23 -0500

    Rubocop

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -2,8 +2,13 @@
 
 module PostsHelper
   def render_links(text)
-    # Don't forget to escape the '.'!
-    permitted_sites = %w[(?:stackoverflow|superuser|serverfault|askubuntu|stackapps)\\.com mathoverflow\\.net \\w*.\\.stackexchange\\.com m(?:etasmoke)?\\.erwaysoftware\\.com]
+    # Don't forget to escape the '.' because regex!
+    permitted_sites = %w[
+      (?:stackoverflow|superuser|serverfault|askubuntu|stackapps)\\.com
+      mathoverflow\\.net
+      \\w*.\\.stackexchange\\.com
+      m(?:etasmoke)?\\.erwaysoftware\\.com
+    ]
     raw(text.split(%r{((?:https?:)?\/{2}(?:#{permitted_sites.join('|')})[^\)\<\s]*)}).map.with_index do |s, i|
       i.even? ? html_escape(s) : link_to(s, s)
     end.join)
