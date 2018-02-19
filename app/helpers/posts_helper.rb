cff460de779b77c5460eea959d8be83f7de42838
izwick-schachter <isaiah@developingtechnician.com>
2018-02-18 23:23:56 -0500

    Fixed typo

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -2,7 +2,7 @@
 
 module PostsHelper
   def render_links(text)
-    raw(reason.split(%r{((?:https?:)?\/{2}[^\)\s]*)}).map.with_index do |s, i|
+    raw(text.split(%r{((?:https?:)?\/{2}[^\)\s]*)}).map.with_index do |s, i|
       i % 2 == 0 ? s : link_to(s, s)
     end.join)
   end
