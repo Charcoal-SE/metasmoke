bff24d5fbc6917a0ca3f4d7f35087594bf936d05
Undo1 <parker@erwaysoftware.com>
2018-03-10 16:45:13 -0700

    Magic!

--- app/controllers/channels_controller.rb
+++ app/controllers/channels_controller.rb
@@ -2,8 +2,14 @@ class ChannelsController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def receive_email
-    Rails.logger.info "debug"
-    Rails.logger.info request.raw_post
-    render text: "hi SNS!"
+    message = JSON.parse(JSON.parse(request.raw_post)["Message"])
+
+    user = message["mail"]["destination"][0].split("@")[0]
+    text = Base64.decode64(message["content"]).gsub("=\r\n", "").gsub("3D", "")
+    link = text.scan(/(https:\/\/stackoverflow.com\/c\/charcoal\/join\/confirmation\?token=[a-z0-9-]{36})/)[0][0]
+
+    SmokeDetector.send_message_to_charcoal("@#{user} Your Channels join link: #{link}")
+
+    render plain: "hi SNS!"
   end
 end
