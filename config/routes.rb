ab83ca8eb77c8eab168d274b6011fa06e1756408
Undo1 <parker@erwaysoftware.com>
2018-03-09 20:30:04 -0700

    Needs to be a POST

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'channels/receive_email'
+  post 'channels/receive_email'
   mount ActionCable.server => '/cable'
 
   root to: 'dashboard#new_dash', as: :dashboard
