6624b354f557f8471cde6506c2a6fe978c4f9dcd
Undo1 <parker@erwaysoftware.com>
2018-03-10 16:54:04 -0700

    Rubocop

--- app/controllers/channels_controller.rb
+++ app/controllers/channels_controller.rb
@@ -1,15 +1,17 @@
+# frozen_string_literal: true
+
 class ChannelsController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def receive_email
-    message = JSON.parse(JSON.parse(request.raw_post)["Message"])
+    message = JSON.parse(JSON.parse(request.raw_post)['Message'])
 
-    user = message["mail"]["destination"][0].split("@")[0]
-    text = Base64.decode64(message["content"]).gsub("=\r\n", "").gsub("3D", "")
-    link = text.scan(/(https:\/\/stackoverflow.com\/c\/charcoal\/join\/confirmation\?token=[a-z0-9-]{36})/)[0][0]
+    user = message['mail']['destination'][0].split('@')[0]
+    text = Base64.decode64(message['content']).gsub('=\r\n', '').gsub('3D', '')
+    link = text.scan(%r{(https:\/\/stackoverflow.com\/c\/charcoal\/join\/confirmation\?token=[a-z0-9-]{36})})[0][0]
 
     SmokeDetector.send_message_to_charcoal("@#{user} Your Channels join link: #{link}")
 
-    render plain: "hi SNS!"
+    render plain: 'hi SNS!'
   end
 end
