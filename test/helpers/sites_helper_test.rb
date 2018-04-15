02a8c50f7c5a3e87648943a5d15ad85cd89f2f8e
ArtOfCode- <hello@artofcode.co.uk>
2018-04-16 00:37:11 +0100

    Correct test stub

--- test/helpers/sites_helper_test.rb
+++ test/helpers/sites_helper_test.rb
@@ -3,7 +3,7 @@
 class SitesHelperTest < ActionView::TestCase
   test 'should get new sites' do
     webmock_json_file = "#{Rails.root}/test/helpers/webmock_json_responses/sites_response.json"
-    stub_request(:get, 'http://api.stackexchange.com/2.2/sites?filter=!*L1-85AFULD6pPxF&pagesize=1000')
+    stub_request(:get, 'https://api.stackexchange.com/2.2/sites?filter=!*L1-85AFULD6pPxF&pagesize=1000')
       .to_return(status: 200, body: File.open(webmock_json_file).read, headers: {})
 
     SitesHelper.update_sites
