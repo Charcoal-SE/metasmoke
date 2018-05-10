dc0be3ea48eb0ddae7f51ed75aaeaf777782205d
ArtOfCode- <hello@artofcode.co.uk>
2018-05-10 21:00:43 +0100

    Make email users confirmable

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
   rolify
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :validatable
 
   has_many :feedbacks
