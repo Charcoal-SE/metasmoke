304e321bf9f16253793e966469c41b0c5dd0ac40
ArtOfCode- <hello@artofcode.co.uk>
2018-05-10 23:36:13 +0100

    Definitely didn't break everything there

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
   rolify
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable, :confirmable,
+  devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   has_many :feedbacks
