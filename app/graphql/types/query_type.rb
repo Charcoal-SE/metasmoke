71bdd06882235d2eb63caed4e7214e1640f6114a
thesecretmaster <thesecretmaster@developingtechnician.com>
2018-05-11 21:15:18 -0400

    DRYed the shit out of query types

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -62,54 +62,35 @@ Types::QueryType = GraphQL::ObjectType.define do
     end
   end
 
-  field :feedback do
-    type Types::FeedbackType
-    argument :id, !types.ID
-    description 'Find a Feedback by ID'
-    resolve ->(_obj, args, _ctx) { Feedback.find(args['id']) }
-  end
-
-  field :smoke_detector do
-    type Types::SmokeDetectorType
-    argument :id, !types.ID
-    description 'Find a SmokeDetector by ID'
-    resolve ->(_obj, args, _ctx) { SmokeDetector.find(args['id']) }
-  end
-
-  field :site do
-    type Types::SiteType
-    argument :id, !types.ID
-    description 'Find a Site by ID'
-    resolve ->(_obj, args, _ctx) { Site.find(args['id']) }
-  end
-
-  field :stack_exchange_user do
-    type Types::StackExchangeUserType
-    argument :id, !types.ID
-    description 'Find a StackExchangeUser by ID'
-    resolve ->(_obj, args, _ctx) { StackExchangeUser.find(args['id']) }
-  end
-
-  field :announcement do
-    type Types::StackExchangeUserType
-    argument :id, !types.ID
-    description 'Find a Announcement by ID'
-    resolve ->(_obj, args, _ctx) { Announcement.find(args['id']) }
-  end
-
-  field :reason do
-    type Types::ReasonType
-    argument :id, !types.ID
-    description 'Find a Reason by ID'
-    resolve ->(_obj, args, _ctx) { Reason.find(args['id']) }
-  end
-
-  field :user do
-    type Types::UserType
-    argument :id, !types.ID
-    description 'Find a User by ID'
-    resolve ->(_obj, args, _ctx) { User.find(args['id']) }
-  end
-
   # deletion_logs domain_tags flag_logs moderator_sites
+
+  def gen_field(name)
+    ar_class = name.camelize.constantize
+    graphql_type = "Types::#{name.camelize}Type".constantize
+
+    field name.to_sym do
+      type graphql_type
+      argument :id, types.ID
+      description "Find one #{ar_class}"
+      resolve ->(_obj, args, _ctx) do
+        ar_class.find(args['id']) if args['id']
+      end
+    end
+
+    field name.pluralize.to_sym do
+      type graphql_type
+      argument :ids, types[types.ID]
+      description "Find multiple #{ar_class.to_s.pluralize}"
+      resolve ->(_obj, args, _ctx) do
+        ar_class.find(args['ids']) if args['ids']
+      end
+    end
+  end
+
+  def gen_fields(*names)
+    names.flatten.each {|name| gen_field(name)}
+  end
+
+  gen_fields %w[feedback smoke_detector site stack_exchange_user announcement
+                reason user flag_log flag_condition]
 end
