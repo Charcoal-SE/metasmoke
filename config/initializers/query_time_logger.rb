9ae37995edc65ee0a6666ff2cc5c398b3970278b
thesecretmaster <thesecretmaster@developingtechnician.com>
2018-05-13 17:39:38 -0400

    Rubocop

--- config/initializers/query_time_logger.rb
+++ config/initializers/query_time_logger.rb
@@ -1,11 +1,13 @@
+# frozen_string_literal: true
+
 require 'json'
 
 logger = Logger.new('log/query_times.log')
 
-logger.formatter = proc do |severity, datetime, progname, msg|
-  JSON.generate({time: datetime, msg: msg})
+logger.formatter = proc do |_severity, datetime, _progname, msg|
+  JSON.generate(time: datetime, msg: msg)
 end
 
 ActiveSupport::Notifications.subscribe('process_action.action_controller') do |**info|
-  logger.info (info)
+  logger.info info
 end
