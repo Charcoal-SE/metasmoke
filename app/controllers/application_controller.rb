21a89cc9484677547dfdb824cc4f977b188c4e38
Undo1 <parker@erwaysoftware.com>
2018-05-13 09:24:01 -0600

    Allow miniprofiler for core

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
   before_action :check_auth_required
 
   before_action do
-    Rack::MiniProfiler.authorize_request if current_user&.has_role?(:developer)
+    Rack::MiniProfiler.authorize_request if current_user&.has_role?(:core)
   end
 
   def check_if_smokedetector
