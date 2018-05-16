df4301552ab030e1b4009aa88cb2730e6775a3e6
ArtOfCode- <hello@artofcode.co.uk>
2018-05-16 01:01:28 +0100

    Model associations

--- app/models/review_item.rb
+++ app/models/review_item.rb
@@ -4,4 +4,5 @@ class ReviewItem < ApplicationRecord
   belongs_to :user
   belongs_to :review_queue
   belongs_to :reviewable, polymorphic: true
+  has_many :results, class_name: 'ReviewResult'
 end
--- app/models/review_queue.rb
+++ app/models/review_queue.rb
@@ -1,4 +1,10 @@
 # frozen_string_literal: true
 
 class ReviewQueue < ApplicationRecord
+  has_many :items, class_name: 'ReviewItem'
+  has_many :results, class_name: 'ReviewResult', through: :items
+
+  def self.[](key)
+    find_by name: key
+  end
 end
