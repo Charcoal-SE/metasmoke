68f34692330e4b5c1eac4e4f719e968570b8cec5
Jonathan Keller <jkeller234@icloud.com>
2018-05-18 12:52:09 -0700

    Fix minor bug when viewing past reviews

--- app/controllers/review_queues_controller.rb
+++ app/controllers/review_queues_controller.rb
@@ -28,8 +28,8 @@ class ReviewQueuesController < ApplicationController
 
     @item = ReviewItem.find params[:item_id]
 
-    # Prevent the same item from being reviewed twice by the same user.
-    if (@item.completed && ReviewResult.exists?(item: @item)) ||
+    # Prevent the same item from being reviewed after it is completed, or twice by the same user.
+    if (@item.completed && ReviewResult.where(item: item).where.not(result: "skip").exists?) ||
        ReviewResult.where(user: current_user, item: @item).where.not(result: 'skip').exists?
 
       render json: { status: 'duplicate' }, status: 409
