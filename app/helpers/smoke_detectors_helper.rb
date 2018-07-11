98458f30de78974fc3bbb557c65d80d4d2a11dce
iBug <stevepad@me.com>
2018-07-11 21:51:13 +0800

    RuboCop

--- app/helpers/smoke_detectors_helper.rb
+++ app/helpers/smoke_detectors_helper.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module SmokeDetectorsHelper
-  def escape_markdown s
+  def escape_markdown(s)
     %w{[ ] * _ `}.each do |token|
       s = s.gsub token, '\\' + token
     end
