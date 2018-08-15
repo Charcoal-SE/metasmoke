95b97c6ea53377dcb976438f3fecbaafd7a1d135
ArtOfCode- <hello@artofcode.co.uk>
2018-08-15 23:23:09 +0100

    Cancel old untagged reviews

---
+++ db/migrate/20180815222054_cancel_old_untagged_reviews.rb
@@ -0,0 +1,6 @@
+class CancelOldUntaggedReviews < ActiveRecord::Migration[5.2]
+  def change
+    # We're not getting them done and it's negatively affecting review speed in other queues, sooo... yup.
+    ReviewQueue['untagged-domains'].items.update_all(completed: true)
+  end
+end
