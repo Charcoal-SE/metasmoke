6580c7c8ddd3e828fcfcdfa5d0d448f9af1d9c39
ArtOfCode- <hello@artofcode.co.uk>
2018-08-18 01:01:30 +0100

    Automatic feedback conflict resolution

--- config/schedule.rb
+++ config/schedule.rb
@@ -21,6 +21,7 @@ every 1.day, at: '2:00 am' do
   runner 'User.where.not(stack_exchange_account_id: nil).each { |u| u.update_moderator_sites }'
   runner 'FlagCondition.revalidate_all'
   runner 'AbuseReport.update_stale_reports'
+  runner 'ConflictingFeedbackJob.perform_later'
 end
 
 every 1.day, at: '2:10am' do
