4ab6d346ba30285c8453e8891248c95f56585166
Undo1 <Undo1@users.noreply.github.com>
2018-09-18 07:24:16 -0600

    Temp remove check_smokey_status

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,9 +1,5 @@
 # frozen_string_literal: true
 
-every 3.minutes do
-  runner 'SmokeDetector.check_smokey_status'
-end
-
 every 1.day do
   runner 'ReasonsHelper.check_for_inactive_reasons'
   runner 'ReasonsHelper.calculate_weights_for_flagging'
