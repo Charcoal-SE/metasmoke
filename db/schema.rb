383fa69e41ba4d12b24cf2370a6734673eda9af1
ArtOfCode- <hello@artofcode.co.uk>
2018-10-23 21:25:40 +0100

    Can't classify a symbol

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_14_124507) do
+ActiveRecord::Schema.define(version: 2018_10_23_122100) do
 
   create_table "abuse_comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci", force: :cascade do |t|
     t.bigint "user_id"
@@ -409,6 +409,7 @@ ActiveRecord::Schema.define(version: 2018_10_14_124507) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "description"
+    t.string "reviewable_type"
   end
 
   create_table "review_results", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci", force: :cascade do |t|
