9aab25a645fa5ee769d395eed5f706f8d1c28540
ArtOfCode- <hello@artofcode.co.uk>
2018-10-23 12:05:54 +0100

    Preload review associations

--- app/controllers/review_queues_controller.rb
+++ app/controllers/review_queues_controller.rb
@@ -65,7 +65,7 @@ class ReviewQueuesController < ApplicationController
 
   def reviews
     @all = params[:all].present? && params[:all] == '1'
-    @reviews = ReviewResult.joins(:item).where(review_items: { review_queue_id: @queue })
+    @reviews = ReviewResult.joins(:item).includes(:item, item: [:reviewable]).where(review_items: { review_queue_id: @queue })
     @reviews = @reviews.where(user: current_user) if @all == false
     @reviews = @reviews.where(user_id: params[:user]) if params[:user].present?
     @reviews = @reviews.where(result: params[:response]) if params[:response].present?
