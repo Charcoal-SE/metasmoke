1c7bcc2d3b295b2a5157e7a454d021410fbd4de2
ArtOfCode <ArtOfCode-@users.noreply.github.com>
2018-11-16 22:43:36 +0000

    Only cast on posts where there were flags

--- app/models/deletion_log.rb
+++ app/models/deletion_log.rb
@@ -31,7 +31,7 @@ class DeletionLog < ApplicationRecord
   def self.auto_other_flag(dl = nil, post = nil)
     return if post.nil? || !post.site&.auto_disputed_flags_enabled
     deleted = dl.present? ? dl.is_deleted : !post.deleted_at.nil?
-    if deleted && (post.is_fp || post.is_naa)
+    if deleted && (post.is_fp || post.is_naa) && post.flag_logs.manual.successful.count > 0
       comment_template = 'This post had {flags} spam flag(s) cast on it by Charcoal members and has since been deleted, but was ultimately judged '\
                          'not to have been spam. Please review whether spam flags - and the penalty that comes with them - are appropriate for this '\
                          'post - you can let us know in https://chat.stackexchange.com/rooms/11540 if the flags were inappropriate. '\
@@ -48,7 +48,8 @@ class DeletionLog < ApplicationRecord
         backoff: status.present? ? message : 0,
         site_id: post.site_id,
         is_auto: true,
-        flag_type: 'other'
+        flag_type: 'other',
+        comment: comment
       )
     end
   end
