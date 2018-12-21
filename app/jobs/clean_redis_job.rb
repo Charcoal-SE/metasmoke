a7c1e526e94df9b1170d3632191f0775fd7ebb0b
thesecretmaster <thesecretmaster@developingtechnician.com>
2018-12-20 20:59:38 -0600

    That's importannt, I should uncomment it

--- app/jobs/clean_redis_job.rb
+++ app/jobs/clean_redis_job.rb
@@ -5,56 +5,56 @@ class CleanRedisJob < ApplicationJob
 
   def perform
     @keys = []
-    # api_key_cache = {} # id => APIKey
-    # thread = Thread.new {}
-    # Post.eager_load(:stack_exchange_user).eager_load(:site).eager_load(:comments).eager_load(:reasons).eager_load(:flag_logs).eager_load(:feedbacks).find_in_batches(batch_size: 250) do |posts|
-    #   thread.join
-    #   thread = Thread.new do
-    #     redis.pipelined do
-    #       posts.each do |post|
-    #         redis.sadd ck('tps'), post.id if post.is_tp
-    #         redis.sadd ck('fps'), post.id if post.is_fp
-    #         redis.sadd ck('naas'), post.id if post.is_naa
-    #         if post.link.nil?
-    #           redis.sadd ck('nolink'), post.id
-    #         else
-    #           redis.sadd ck('questions'), post.id if post.question?
-    #           redis.sadd ck('answers'), post.id if post.answer?
-    #         end
-    #         redis.sadd ck('autoflagged'), post.id if post.flagged?
-    #         redis.sadd ck("sites/#{post.site_id}/posts"), post.id
-    #         redis.sadd ck('all_posts'), post.id
-    #         redis.zadd ck('posts'), post.created_at.to_i, post.id
-    #         post_hash = {
-    #           body: post.body,
-    #           title: post.title,
-    #           reason_weight: post.reasons.map(&:weight).reduce(:+),
-    #           created_at: post.created_at,
-    #           username: post.username,
-    #           link: post.link,
-    #           site_site_logo: post.site.try(:site_logo),
-    #           stack_exchange_user_username: post.stack_exchange_user.try(:username),
-    #           stack_exchange_user_id: post.stack_exchange_user.try(:id),
-    #           flagged: post.flagged?,
-    #           site_id: post.site_id,
-    #           post_comments_count: post.comments.length, # post.comments.count,
-    #           why: post.why
-    #         }
-    #         redis.hmset("posts/#{post.id}", *post_hash.to_a)
-    #
-    #         reason_names = post.reasons.map(&:reason_name)
-    #         reason_weights = post.reasons.map(&:weight)
-    #         redis.zadd(ck("posts/#{post.id}/reasons"), reason_weights.zip(reason_names)) unless post.reasons.empty?
-    #
-    #         post.feedbacks(&:populate_redis)
-    #         nil
-    #       end
-    #       nil
-    #     end
-    #   end
-    #   GC.start
-    #   nil
-    # end
+    api_key_cache = {} # id => APIKey
+    thread = Thread.new {}
+    Post.eager_load(:stack_exchange_user).eager_load(:site).eager_load(:comments).eager_load(:reasons).eager_load(:flag_logs).eager_load(:feedbacks).find_in_batches(batch_size: 250) do |posts|
+      thread.join
+      thread = Thread.new do
+        redis.pipelined do
+          posts.each do |post|
+            redis.sadd ck('tps'), post.id if post.is_tp
+            redis.sadd ck('fps'), post.id if post.is_fp
+            redis.sadd ck('naas'), post.id if post.is_naa
+            if post.link.nil?
+              redis.sadd ck('nolink'), post.id
+            else
+              redis.sadd ck('questions'), post.id if post.question?
+              redis.sadd ck('answers'), post.id if post.answer?
+            end
+            redis.sadd ck('autoflagged'), post.id if post.flagged?
+            redis.sadd ck("sites/#{post.site_id}/posts"), post.id
+            redis.sadd ck('all_posts'), post.id
+            redis.zadd ck('posts'), post.created_at.to_i, post.id
+            post_hash = {
+              body: post.body,
+              title: post.title,
+              reason_weight: post.reasons.map(&:weight).reduce(:+),
+              created_at: post.created_at,
+              username: post.username,
+              link: post.link,
+              site_site_logo: post.site.try(:site_logo),
+              stack_exchange_user_username: post.stack_exchange_user.try(:username),
+              stack_exchange_user_id: post.stack_exchange_user.try(:id),
+              flagged: post.flagged?,
+              site_id: post.site_id,
+              post_comments_count: post.comments.length, # post.comments.count,
+              why: post.why
+            }
+            redis.hmset("posts/#{post.id}", *post_hash.to_a)
+
+            reason_names = post.reasons.map(&:reason_name)
+            reason_weights = post.reasons.map(&:weight)
+            redis.zadd(ck("posts/#{post.id}/reasons"), reason_weights.zip(reason_names)) unless post.reasons.empty?
+
+            post.feedbacks(&:populate_redis)
+            nil
+          end
+          nil
+        end
+      end
+      GC.start
+      nil
+    end
 
     redis.pipelined do
       DeletionLog.eager_load(:post).find_each do |dl|
