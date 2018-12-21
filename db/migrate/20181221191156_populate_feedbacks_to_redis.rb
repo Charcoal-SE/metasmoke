0fabed0e47c246143fef6b821e0d7a0f87211de2
thesecretmaster <thesecretmaster@developingtechnician.com>
2018-12-21 14:02:53 -0600

    Fix migration so travis is happy

--- db/migrate/20181221191156_populate_feedbacks_to_redis.rb
+++ db/migrate/20181221191156_populate_feedbacks_to_redis.rb
@@ -1,5 +1,7 @@
 class PopulateFeedbacksToRedis < ActiveRecord::Migration[5.2]
   def change
+    # If this line isn't here, travis thinks we died
+    ActiveRecord::Base.logger = Logger.new(STDOUT)
     Feedback.populate_redis_meta
   end
 end
