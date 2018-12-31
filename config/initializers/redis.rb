03a2ee522bac0fad1e4cf969e7faf9d41420a218
thesecretmaster <thesecretmaster@developingtechnician.com>
2018-12-31 09:28:45 -0600

    Add redis logs (log/redis.log), add redis runtime to db runtime, and view fixes (css + s/s/ms)

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,9 +1,46 @@
 # frozen_string_literal: true
 
 require 'redis'
+require 'active_record'
+
+# In theory, I should PR the authors of redis-rb with this, but I'm lazy.
+class Redis::Client
+  protected
+
+  def logging(commands)
+    return yield unless @logger && @logger.debug?
+
+    begin
+      commands.each do |name, *args|
+        logged_args = args.map do |a|
+          case
+          when a.respond_to?(:inspect) then a.inspect
+          when a.respond_to?(:to_s)    then a.to_s
+          else
+            # handle poorly-behaved descendants of BasicObject
+            klass = a.instance_exec { (class << self; self end).superclass }
+            "\#<#{klass}:#{a.__id__}>"
+          end
+        end
+        @logger.debug("[Redis] command=#{name.to_s.upcase} args=#{logged_args.join(' ')}")
+      end
+
+      t1 = Time.now
+      yield
+    ensure
+      if t1
+        call_time = (Time.now - t1) * 1000
+        puts "BEFORE RUNTIME: #{ActiveRecord::RuntimeRegistry.sql_runtime}"
+        ActiveRecord::RuntimeRegistry.sql_runtime = ActiveRecord::RuntimeRegistry.sql_runtime.to_i + call_time
+        puts "AFTER RUNTIME: #{ActiveRecord::RuntimeRegistry.sql_runtime}"
+        @logger.debug("[Redis] call_time=%0.2f ms" % (call_time))
+      end
+    end
+  end
+end
 
 def redis
-  $redis ||= Redis.new(AppConfig['redis'])
+  $redis ||= Redis.new(AppConfig['redis'].merge(logger: Logger.new(File.join(Rails.root, 'log', 'redis.log'))))
 end
 
 def with_no_score(ary)
