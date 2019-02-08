e47db14e7b50299c9968846c550dde3014aba7df
thesecretmaster <thesecretmaster@developingtechnician.com>
2019-02-08 09:52:10 -0600

    "consistancy" _grumble_

--- app/models/redis/site_setting.rb
+++ app/models/redis/site_setting.rb
@@ -3,19 +3,17 @@
 class Redis::SiteSetting
   PREFIX = 'site_setting'
 
-  class << self
-    def bool(name)
-      redis.get("#{PREFIX}/#{name}").nil? ? false : true
-    end
+  def self.bool(name)
+    redis.get("#{PREFIX}/#{name}").nil? ? false : true
+  end
 
-    def get(name)
-      redis.get "#{PREFIX}/#{name}"
-    end
+  def self.get(name)
+    redis.get "#{PREFIX}/#{name}"
+  end
 
-    def set(name, val)
-      v = 1 if val == true
-      v = nil if val == false
-      redis.set "#{PREFIX}/#{name}", v
-    end
+  def self.set(name, val)
+    v = 1 if val == true
+    v = nil if val == false
+    redis.set "#{PREFIX}/#{name}", v
   end
 end
