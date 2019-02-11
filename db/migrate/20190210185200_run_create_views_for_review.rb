b8a9e57a5598f8902824e2e955d9ca9497c90d8e
Undo1 <parker@erwaysoftware.com>
2019-02-11 11:08:55 -0700

    Revert "Add review tables to SQLEx"
    This reverts commit 20f5fd155505ea01d4c9dda570e72f0999f97dda.

--- db/migrate/20190210185200_run_create_views_for_review.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class RunCreateViewsForReview < ActiveRecord::Migration[5.2]
-  def change
-    success = system 'rails runner db/scripts/create_views.rb'
-    raise StandardError, 'View script had non-zero exit code' unless success
-  end
-end
