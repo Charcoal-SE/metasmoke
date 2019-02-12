e9b7580d25c2e05fb929773e587ea0f76314bd1c
ArtOfCode- <hello@artofcode.co.uk>
2019-02-12 01:51:15 +0000

    Surface reason descriptions on #show, close #587

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_11_194432) do
+ActiveRecord::Schema.define(version: 2019_02_12_003856) do
 
   create_table "abuse_comments", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
     t.bigint "user_id"
