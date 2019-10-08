b3d67199e08b95088dc1d39548fbf2902e7e9453
ArtOfCode- <hello@artofcode.co.uk>
2019-10-09 00:36:23 +0100

    Create domain group summary email template
    Still to go:
     * Controllers and views for managing email preferences and unsubscriptions
     * Views within domain groups for admins to subscribe people to emails

--- app/mailers/domain_groups_mailer.rb
+++ app/mailers/domain_groups_mailer.rb
@@ -6,9 +6,9 @@ class DomainGroupsMailer < ApplicationMailer
     @addressee = preference.addressee
     @group = preference.reference
 
-    last_email_date = (Date.today - preference.frequency.days).to_time.iso8601
+    @last_email_date = (Date.today - preference.frequency.days).to_time.iso8601
     @domains = DomainGroup.where(id: @group.id).joins(Arel.sql('INNER JOIN domain_groups_spam_domains j ON j.domain_group_id = domain_groups.id'))
-                          .where('j.created_at >= ?', last_email_date)
+                          .where('j.created_at >= ?', @last_email_date)
 
     @preference.update(next: (Date.today + preference.frequency.days).to_time.iso8601)
 
