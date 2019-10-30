78bcf075dd6947056b9329eea4f445f797587f83
ArtOfCode- <hello@artofcode.co.uk>
2019-10-30 22:26:28 +0000

    Broadcast updates to the whitelist tag

--- app/models/domain_tag.rb
+++ app/models/domain_tag.rb
@@ -3,12 +3,18 @@
 class DomainTag < ApplicationRecord
   include Websocket
 
-  has_and_belongs_to_many :spam_domains
+  has_and_belongs_to_many :spam_domains, after_add: :send_whitelist
   has_and_belongs_to_many :posts
   has_many :abuse_reports, as: :reportable
 
   scope(:standard, -> { where(special: false) })
   scope(:special, -> { where(special: true) })
+
+  def send_whitelist
+    if self.name == 'whitelisted'
+      ActionCable.server.broadcast 'smokedetector_messages', domain_whitelist: 'refresh'
+    end
+  end
 
   def merge_into(master)
     master.spam_domains += (spam_domains.to_a - master.spam_domains.to_a)
