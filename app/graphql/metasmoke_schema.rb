a2df5c1630c90e362621cc58c27cd74023933bf2
Makyen <wp.makyen@gmail.com>
2020-01-02 18:10:49 -0800

    GraphiQL performs a couple of queries which have a depth of 13

--- app/graphql/metasmoke_schema.rb
+++ app/graphql/metasmoke_schema.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 MetasmokeSchema = GraphQL::Schema.define do
-  max_depth 6
+  max_depth 15
   max_complexity 5000
   # mutation(Types::MutationType)
   query(Types::QueryType)
