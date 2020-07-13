21c2b79b9f999d096c2317aee771e93138728e49
thesecretmaster <thesecretmaster@developingtechnician.com>
2020-07-13 13:17:32 -0500

    Fix code review stuff

--- app/models/post.rb
+++ app/models/post.rb
@@ -28,7 +28,7 @@ class Post < ApplicationRecord
   has_and_belongs_to_many :reasons
   has_and_belongs_to_many :post_tags, class_name: 'DomainTag'
   has_many :post_spam_domains
-  has_and_belongs_to_many :spam_domains
+  has_many :spam_domains, through: :post_spam_domains
   has_many :feedbacks, dependent: :destroy
   has_many :deletion_logs, dependent: :destroy
   has_many :flag_logs, dependent: :destroy
