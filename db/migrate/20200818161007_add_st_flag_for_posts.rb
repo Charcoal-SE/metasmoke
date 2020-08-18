05744370aa027cdb47d370f8f79a3925c0143ce4
user12986714 <65436504+user12986714@users.noreply.github.com>
2020-08-18 12:21:53 -0400

    Rename migration class properly

--- db/migrate/20200818161007_add_st_flag_for_posts.rb
+++ db/migrate/20200818161007_add_st_flag_for_posts.rb
@@ -1,4 +1,4 @@
-class AddSTFlagForPosts < ActiveRecord::Migration[5.2]
+class AddStFlagForPosts < ActiveRecord::Migration[5.2]
   def change
     add_column :posts, :st_indexed, :boolean, default: false, null: false
   end
