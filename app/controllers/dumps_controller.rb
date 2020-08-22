7cf76d0a07585b512b60aa801ff9fa1dc7614fe3
Thomas Ward <teward@ubuntu.com>
2020-08-21 22:15:08 -0400

    Fix DB dumps YAML path
    Actually make this work with the actual path heh.

--- app/controllers/dumps_controller.rb
+++ app/controllers/dumps_controller.rb
@@ -6,7 +6,7 @@ class DumpsController < ApplicationController
   # GET /dumps
   # GET /dumps.json
   def index
-    dump_list = YAML.load_file('/var/rails/metasmoke/shared/dumps/files.yml')
+    dump_list = YAML.load_file('/var/railsapps/metasmoke/shared/dumps')
     @dumps = dump_list['database'].map { |k, v| { name: "#{k}.sql", url: v } }
     @redis_dumps = dump_list['redis'].map { |k, v| { name: "#{k}.rdb", url: v } }
   end
