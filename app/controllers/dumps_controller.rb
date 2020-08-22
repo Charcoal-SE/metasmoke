fff05e6bd03bbe09dedc49245cc4e845663feed4
Thomas Ward <teward@ubuntu.com>
2020-08-21 22:17:27 -0400

    Use file path, not dir path
    Copy/paste failure

--- app/controllers/dumps_controller.rb
+++ app/controllers/dumps_controller.rb
@@ -6,7 +6,7 @@ class DumpsController < ApplicationController
   # GET /dumps
   # GET /dumps.json
   def index
-    dump_list = YAML.load_file('/var/railsapps/metasmoke/shared/dumps')
+    dump_list = YAML.load_file('/var/railsapps/metasmoke/shared/dumps/files.yml')
     @dumps = dump_list['database'].map { |k, v| { name: "#{k}.sql", url: v } }
     @redis_dumps = dump_list['redis'].map { |k, v| { name: "#{k}.rdb", url: v } }
   end
