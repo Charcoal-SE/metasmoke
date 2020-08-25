af21409899719ef69a44df25e8651356ed91bc0c
user12986714 <65436504+user12986714@users.noreply.github.com>
2020-08-24 22:42:17 -0400

    Implement proper logging for daily sync job

--- app/jobs/sync_suffix_tree_job.rb
+++ app/jobs/sync_suffix_tree_job.rb
@@ -4,6 +4,12 @@ class SyncSuffixTreeJob < ApplicationJob
   queue_as :default
 
   def perform
-    SuffixTreeHelper.sync!
+    Rails.logger.info "Job started (#{DateTime.now}, +0s)"
+    if SuffixTreeHelper.functional?
+      SuffixTreeHelper.sync!
+    else
+      Rails.logger.warn "Suffix tree not functional; reason: #{SuffixTreeHelper.broken_reason}. Not performing msync."
+    end
+    Rails.logger.info "Job finished (#{DateTime.now})"
   end
 end
