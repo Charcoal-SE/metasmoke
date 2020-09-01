197985cd8de76cb5d9835dd89cb97fcbbdddd774
user12986714 <65436504+user12986714@users.noreply.github.com>
2020-08-31 23:38:21 -0400

    Remove useless timestamps

--- app/jobs/insert_posts_to_suffix_tree_job.rb
+++ app/jobs/insert_posts_to_suffix_tree_job.rb
@@ -4,10 +4,8 @@ class InsertPostsToSuffixTreeJob < ApplicationJob
   queue_as :default
 
   def perform(*post_ids)
-    Rails.logger.info "Job started (#{DateTime.now}, +0s)"
     post_ids.flatten.each do |id|
       SuffixTreeHelper.insert_post id
     end
-    Rails.logger.info "Job finished (#{DateTime.now})"
   end
 end
--- app/jobs/sync_suffix_tree_job.rb
+++ app/jobs/sync_suffix_tree_job.rb
@@ -4,12 +4,10 @@ class SyncSuffixTreeJob < ApplicationJob
   queue_as :default
 
   def perform
-    Rails.logger.info "Job started (#{DateTime.now}, +0s)"
     if SuffixTreeHelper.functional?
       SuffixTreeHelper.sync!
     else
       Rails.logger.warn "Suffix tree not functional; reason: #{SuffixTreeHelper.broken_reason}. Not performing msync."
     end
-    Rails.logger.info "Job finished (#{DateTime.now})"
   end
 end
