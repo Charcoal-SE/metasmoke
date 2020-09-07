6275377c3260e243ad34c068ca7fc8f782aca646
user12986714 <65436504+user12986714@users.noreply.github.com>
2020-09-06 21:53:26 -0400

    Remove more obsolete/never used methods

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -3,11 +3,6 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 
-  def self.fuzzy_search(term, **cols)
-    sanitized = sanitize_for_search term, **cols
-    select(Arel.sql("`#{table_name}`.*, #{sanitized} AS search_score"))
-  end
-
   def self.match_search(term, with_search_score: true, **cols)
     sanitized = sanitize_for_search term, **cols
     if with_search_score
@@ -35,15 +30,6 @@ class ApplicationRecord < ActiveRecord::Base
     end.join(', ')
 
     ActiveRecord::Base.send(:sanitize_sql_array, ["MATCH (#{cols}) AGAINST (? IN BOOLEAN MODE)", term])
-  end
-
-  # From http://stackoverflow.com/questions/6591722/how-to-generate-fixtures-based-on-my-development-database
-  def dump_fixture
-    fixture_file = "#{Rails.root}/test/fixtures/#{self.class.table_name}.yml"
-    File.open(fixture_file, 'a') do |f|
-      f.puts({ "#{self.class.table_name.singularize}_#{id}" => attributes }
-        .to_yaml.sub!(/---\s?/, "\n"))
-    end
   end
 
   def self.sanitize_like(unsafe, *args)
