40248fccbf7f014272cafa4c3a4ff3047cbbd3ac
user <65436504+user12986714@users.noreply.github.com>
2021-03-24 17:02:17 -0400

    Fix linting errors

--- app/javascript/feedbacks.js
+++ app/javascript/feedbacks.js
@@ -1,27 +1,27 @@
 const clickHandler = () => {
-    // If one of the feedback buttons are clicked, disable **ALL**
-    // those buttons (which are actually <a> elements).
-    document.querySelectorAll('.feedback-button').forEach((e) => {
-        e.tabIndex = -1
-        e.setAttribute('data-disabled', 'disabled')
-    })
-}
+  // If one of the feedback buttons are clicked, disable **ALL**
+  // those buttons (which are actually <a> elements).
+  document.querySelectorAll('.feedback-button').forEach(e => {
+    e.tabIndex = -1;
+    e.setAttribute('data-disabled', 'disabled');
+  });
+};
 
 const onLoadHandler = () => {
-    document.querySelectorAll('.feedback-button').forEach((e) => {
-        e.addEventListener('click', clickHandler)
-    })
-}
+  document.querySelectorAll('.feedback-button').forEach(e => {
+    e.addEventListener('click', clickHandler);
+  });
+};
 
 const onBeforeCacheHandler = () => {
-    document.querySelectorAll('.feedback-button').forEach((e) => {
-        e.removeEventListener('click', clickHandler)
-        e.tabIndex = 0
-        e.removeAttribute('data-disabled')
-    })
-    document.removeEventListener('turbolinks:load', onLoadHandler)
-    document.removeEventListener('turbolinks:before-cache', onBeforeCacheHandler)
-}
+  document.querySelectorAll('.feedback-button').forEach(e => {
+    e.removeEventListener('click', clickHandler);
+    e.tabIndex = 0;
+    e.removeAttribute('data-disabled');
+  });
+  document.removeEventListener('turbolinks:load', onLoadHandler);
+  document.removeEventListener('turbolinks:before-cache', onBeforeCacheHandler);
+};
 
-document.addEventListener('turbolinks:load', onLoadHandler)
-document.addEventListener('turbolinks:before-cache', onBeforeCacheHandler)
+document.addEventListener('turbolinks:load', onLoadHandler);
+document.addEventListener('turbolinks:before-cache', onBeforeCacheHandler);
