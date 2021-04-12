c4fbe5700b5b1171379632a64e174a4d28606bb2
user12986714 <65436504+user12986714@users.noreply.github.com>
2021-04-12 13:19:58 -0400

    Backout e71ca06 and delete the appropriate version

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -29,6 +29,22 @@ class ApplicationController < ActionController::Base
     raise ActionController::RoutingError, 'Not Found'
   end
 
+  def after_sign_in_path_for(resource_or_scope)
+    if current_user.username.nil?
+      users_username_path
+    elsif current_user.stack_exchange_account_id.nil?
+      authentication_status_path
+    else
+      stored_location = nil
+      begin
+        stored_location = stored_location_for(resource_or_scope)
+      rescue # rubocop:disable Lint/HandleExceptions
+      end
+
+      request.env['omniauth.origin'] || stored_location || root_path
+    end
+  end
+
   def redis_log(_msg)
     # redis = redis(logger: true)
     # redis.multi do |m|
@@ -127,9 +143,5 @@ class ApplicationController < ActionController::Base
 
   def location_storable?
     request.get? && is_navigational_format? && !devise_controller? && !request.xhr?
-  end
-
-  def after_sign_in_path_for(resource)
-    stored_location_for(resource) || super
   end
 end
