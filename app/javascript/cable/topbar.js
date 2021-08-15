3b4a8b808e8d20ed99bebd07e5071e3b97e4eb1e
Makyen <wp.makyen@gmail.com>
2021-08-15 09:34:35 -0700

    Topbar SD status: only update when there are changes.
    Reduce the frequency at which UI updates are performed
    to just those where the status might have changed.
    The DOM is only changed when there are differences the user
    could see.

--- app/javascript/cable/topbar.js
+++ app/javascript/cable/topbar.js
@@ -23,30 +23,60 @@ $(() => {
       }
       if (lastPing != null) {
         $('.navbar .status').data('last-ping', lastPing);
+        // Update the display for the new last ping time.
+        updateStatus();
       }
     }
   });
 
-  setInterval(() => {
+  function setOnlyStatusClassIfNotHasClass(el, statusClass) {
+    if (!el.hasClass(statusClass)) {
+      el.removeClass('status-good status-warning status-critical')
+        .addClass(statusClass);
+    }
+  }
+
+  let updateStatusTimer = 0;
+  function updateStatus() {
+    clearTimeout(updateStatusTimer);
+    if (document.hidden) {
+      // If the document is hidden, then there isn't much benefit from updating the UI. The
+      //   UI will be updated when the visibility of the page changes.
+      return;
+    }
     const $status = $('.navbar #smokedetector-status');
     const lastPing = parseFloat($status.data('last-ping')) * 1e3;
     const ago = Date.now() - lastPing;
     const title = `Last ping was ${moment(lastPing).fromNow()}.`;
     let status = 'critical';
+    // The .fromNow() method only provides minute resolution. So time passing can cause the status text
+    //   to change change at the earliest at the point when "ago" passes the next minute.
+    let msToNextPossibleChange = 60e3 - (ago % 60e3); // Time to next minute ago;
     if (ago < 90e3) {
       status = 'good';
+      msToNextPossibleChange = Math.min(msToNextPossibleChange, 90e3 - ago); // Time to 1.5 minutes ago, if less;
     }
     else if (ago < 3 * 60e3) {
       status = 'warning';
+      msToNextPossibleChange = Math.min(msToNextPossibleChange, 3 * 60e3 - ago); // Time to 3 minutes ago, if less;
     }
-    $('.navbar-toggle').removeClass('status-good status-warning status-critical')
-                       .addClass(`status-${status}`);
-    $status.removeClass('status-good status-warning status-critical')
-           .addClass(`status-${status}`)
-           .attr('data-original-title', title)
-           .tooltip()
-           .parent()
+    const navbarToggle = $('.navbar-toggle');
+    const statusClass = `status-${status}`;
+    setOnlyStatusClassIfNotHasClass(navbarToggle, statusClass);
+    setOnlyStatusClassIfNotHasClass($status, statusClass);
+    const currentOrigTitle = $status.attr('data-original-title');
+    const currentTooltipInnerText = $status.find('.status + .tooltip .tooltip-inner')
+                                           .text();
+    if (currentOrigTitle !== title || currentTooltipInnerText !== title) {
+      $status.attr('data-original-title', title)
+             .tooltip()
+             .parent()
              .find('.status + .tooltip .tooltip-inner')
              .text(title);
-  }, 1000);
+    }
+    // Get called again just after when anything displayed to the user might update.
+    updateStatusTimer = setTimeout(updateStatus, msToNextPossibleChange + 1);
+  }
+  updateStatus();
+  window.addEventListener('visibilitychange', updateStatus);
 });
