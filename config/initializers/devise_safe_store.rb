d7b7f7050830b72a4d703508779005334ef73ff1
ArtOfCode- <ArtOfCode-@users.noreply.github.com>
2021-11-20 20:29:32 +0000

    Try to fix cookie overflow

---
+++ config/initializers/devise_safe_store.rb
@@ -0,0 +1,11 @@
+module SafeStoreLocation
+
+  MAX_LOCATION_SIZE = ActionDispatch::Cookies::MAX_COOKIE_SIZE / 2
+
+  def store_location_for(resource_or_scope, location)
+    super unless location && location.size > MAX_LOCATION_SIZE
+  end
+
+end
+
+Devise::FailureApp.include SafeStoreLocation
