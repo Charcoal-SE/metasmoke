e5cf4825934f2c2e01edfb8217ad22e097448738
Makyen <makyen@users.noreply.github.com>
2022-08-18 21:20:04 -0700

    Add the dup bootstrap-select removal to onLoad() after others.

--- app/javascript/general_fixes.js
+++ app/javascript/general_fixes.js
@@ -1,13 +1,16 @@
 import { onLoad } from './util';
 
-onLoad(() => {
-  // This is a patch to remove duplicated dropdown selections.
-  // It would be better to prevent these from being created.
-  setTimeout(() => {
+setTimeout(() => {
+  /* This is a patch to remove duplicated bootstrap-select dropdown selections.
+   * It would be better to prevent these from being created.
+   * This is added to onLoad() from a setTimeout, so it ends up being called
+   * after other functions. Putting the operation within a setTimeout that's
+   * called on each load results in the duplicate being visible briefly.
+   */
+  onLoad(() => {
     $('.dropdown.bootstrap-select > .dropdown.bootstrap-select').each(function () {
       const useSelect = $(this);
       useSelect.parent().replaceWith(useSelect);
     });
-  }, 10);
-});
-
+  });
+}, 100);
