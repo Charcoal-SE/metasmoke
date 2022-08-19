d1e0d7dd4770c3a28928d54c17cf28baeabf1b8b
Makyen <makyen@users.noreply.github.com>
2022-08-18 21:01:35 -0700

    Delay removing dup bootstrap-select until after other onLoad().

--- app/javascript/general_fixes.js
+++ app/javascript/general_fixes.js
@@ -3,9 +3,11 @@ import { onLoad } from './util';
 onLoad(() => {
   // This is a patch to remove duplicated dropdown selections.
   // It would be better to prevent these from being created.
-  $('.dropdown.bootstrap-select > .dropdown.bootstrap-select').each(function () {
-    const useSelect = $(this);
-    useSelect.parent().replaceWith(useSelect);
-  });
+  setTimeout(() => {
+    $('.dropdown.bootstrap-select > .dropdown.bootstrap-select').each(function () {
+      const useSelect = $(this);
+      useSelect.parent().replaceWith(useSelect);
+    });
+  }, 10);
 });
 
