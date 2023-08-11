d49dbddefde774094a6986472e4865788db9739d
Makyen <makyen@users.noreply.github.com>
2023-08-10 18:08:09 -0700

    Account for malformed access_token values
    2023-08-10 SE started sending
      <token>&scope=write_access%20no_expiry
    for the access_token, at least for the implicit OAuth 2.0 flow instead of the expected
      <token>
    
    The change is causing it to be impossible to log into MS.
    
    This isn't yet verified for the explicit OAuth 2.0 flow which MS is using. Making this change
    should, effectively, verify that it's happening the same way with the explicit flow.

--- app/helpers/authentication_helper.rb
+++ app/helpers/authentication_helper.rb
@@ -39,7 +39,12 @@ module AuthenticationHelper
     }.stringify_keys
     response = Rack::Utils.parse_nested_query(Net::HTTP.post_form(URI.parse('https://stackexchange.com/oauth/access_token'), request_params).body)
 
-    response['access_token']
+    # 2023-08-10 SE started sending
+    #   <token>&scope=write_access%20no_expiry
+    # for the access_token, at least for the implicit OAuth 2.0 flow instead of the expected
+    #   <token>
+    # The following should work for both.
+    response['access_token'].sub(/&.*/, '')
   end
 
   def info_for_access_token(access_token)
